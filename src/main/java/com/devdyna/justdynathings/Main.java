d529cc46a62bf835138ba12053de3200372b450e
DevDyna <andrearisorti2003@gmail.com>
2025-01-23 23:09:51 +0100

    Update Main.java

--- src/main/java/com/devdyna/justdynathings/Main.java
+++ src/main/java/com/devdyna/justdynathings/Main.java
@@ -1,11 +1,11 @@
 package com.devdyna.justdynathings;
 
-import com.devdyna.justdynathings.init.Config;
+// import com.devdyna.justdynathings.init.Config;
 import com.devdyna.justdynathings.init.Reg;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
+// import net.neoforged.fml.config.ModConfig;
 
 @Mod(Main.ID)
 public class Main {
@@ -13,7 +13,7 @@ public class Main {
 
     public Main(IEventBus modEventBus, ModContainer modContainer) {
         Reg.register(modEventBus);
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+        // modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
 
         // if (FMLLoader.getDist().isClient()) {
         //     modEventBus.addListener(Client::init);

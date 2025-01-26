8d89e2d8043595fac99c880072117329ad26c0b6
DevDyna <andrearisorti2003@gmail.com>
2025-01-27 00:42:08 +0100

    Create GooBlockItem.java

---
+++ src/main/java/com/devdyna/justdynathings/simply/GooBlockItem.java
@@ -0,0 +1,11 @@
+package com.devdyna.justdynathings.simply;
+
+import com.direwolf20.justdirethings.common.blocks.gooblocks.GooBlock_Item;
+
+import net.minecraft.world.level.block.Block;
+
+public class GooBlockItem extends GooBlock_Item {
+    public GooBlockItem(Block block) {
+        super(block, new Properties());
+    }
+}

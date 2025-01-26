b1afb6e50376bbaae11069efb7cd3c0dbdc14a85
DevDyna <andrearisorti2003@gmail.com>
2025-01-26 17:25:32 +0100

    Update Main.java

--- src/main/java/com/devdyna/justdynathings/Main.java
+++ src/main/java/com/devdyna/justdynathings/Main.java
@@ -1,5 +1,6 @@
 package com.devdyna.justdynathings;
 
+// import com.devdyna.justdynathings.events.BlockClick;
 import com.devdyna.justdynathings.init.material;
 import com.devdyna.justdynathings.init.tab;
 
@@ -7,6 +8,7 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 // import net.neoforged.fml.config.ModConfig;
+// import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(Main.ID)
 public class Main {
@@ -15,10 +17,6 @@ public class Main {
     public Main(IEventBus modEventBus, ModContainer modContainer) {
         material.register(modEventBus);
         tab.register(modEventBus);
-        // modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-
-        // if (FMLLoader.getDist().isClient()) {
-        //     modEventBus.addListener(Client::init);
-        // }
+        // NeoForge.EVENT_BUS.register(new BlockClick());
     }
 }

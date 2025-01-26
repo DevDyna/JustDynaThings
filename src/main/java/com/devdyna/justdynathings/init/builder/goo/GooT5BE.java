5775eecf5222edd30b0bf6170e533e5d2bd0e3e5
DevDyna <andrearisorti2003@gmail.com>
2025-01-27 00:42:03 +0100

    Update GooT5BE.java

--- src/main/java/com/devdyna/justdynathings/init/builder/goo/GooT5BE.java
+++ src/main/java/com/devdyna/justdynathings/init/builder/goo/GooT5BE.java
@@ -1,7 +1,7 @@
 package com.devdyna.justdynathings.init.builder.goo;
 
 
-import com.devdyna.justdynathings.init.material;
+import com.devdyna.justdynathings.init.Material;
 import com.direwolf20.justdirethings.common.blockentities.basebe.GooBlockBE_Base;
 
 import net.minecraft.core.BlockPos;
@@ -10,7 +10,7 @@ import net.minecraft.world.level.block.state.BlockState;
 public class GooT5BE extends GooBlockBE_Base {
 
    public GooT5BE(BlockPos pos, BlockState state) {
-       super(material.GooT5_BE.get(), pos, state);
+       super(Material.GooT5_BE.get(), pos, state);
     }
  
     public int getTier() {

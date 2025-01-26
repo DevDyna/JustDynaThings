f4c05392f29f6ce65d57ae1b60b6720642140c79
DevDyna <andrearisorti2003@gmail.com>
2025-01-27 00:42:11 +0100

    Create RawOre.java

---
+++ src/main/java/com/devdyna/justdynathings/simply/RawOre.java
@@ -0,0 +1,15 @@
+package com.devdyna.justdynathings.simply;
+
+import com.direwolf20.justdirethings.common.blocks.baseblocks.BaseRawOre;
+import net.minecraft.world.level.block.SoundType;
+
+public class RawOre extends BaseRawOre {
+    public RawOre(SoundType s,float destroytime,float explosiondestroy) {
+        super(Properties.of()
+                .sound(s)
+                .requiresCorrectToolForDrops()
+                .noOcclusion()
+                .strength(destroytime,explosiondestroy)
+        );
+    }
+}

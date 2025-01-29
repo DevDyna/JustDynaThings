c76fa48ab7bb3901d700f00f264c7ca39c2503e4
DevDyna <andrearisorti2003@gmail.com>
2025-01-29 01:35:31 +0100

    Create Client.java
    not yet working but an init

---
+++ src/main/java/com/devdyna/justdynathings/init/Client.java
@@ -0,0 +1,26 @@
+package com.devdyna.justdynathings.init;
+
+import com.devdyna.justdynathings.Main;
+import com.devdyna.justdynathings.init.builder.goo.GooT0Render;
+import com.devdyna.justdynathings.init.builder.goo.GooT5Render;
+import com.devdyna.justdynathings.init.builder.oreGen.GenScreen;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
+
+@EventBusSubscriber(modid = Main.ID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
+public class Client {
+
+    @SubscribeEvent
+    public static void registerScreens(RegisterMenuScreensEvent event) {
+        event.register(Material.GEN_GUI.get(), GenScreen::new);
+    }
+
+    @SubscribeEvent
+    public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerBlockEntityRenderer(Material.GooT0_BE.get(), GooT0Render::new);
+        event.registerBlockEntityRenderer(Material.GooT5_BE.get(), GooT5Render::new);
+    }
+}

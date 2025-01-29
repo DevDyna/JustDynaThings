6940161803c7d7f8f6fde29b54ca14a3bc75368d
DevDyna <andrearisorti2003@gmail.com>
2025-01-29 01:35:40 +0100

    Create GooT5Render.java

---
+++ src/main/java/com/devdyna/justdynathings/init/builder/goo/GooT5Render.java
@@ -0,0 +1,11 @@
+package com.devdyna.justdynathings.init.builder.goo;
+
+import com.direwolf20.justdirethings.client.blockentityrenders.baseber.GooBlockRender_Base;
+import com.direwolf20.justdirethings.common.blockentities.basebe.GooBlockBE_Base;
+
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+
+public class GooT5Render extends GooBlockRender_Base<GooBlockBE_Base> {
+    public GooT5Render(BlockEntityRendererProvider.Context context) {
+       super(context);
+    }}

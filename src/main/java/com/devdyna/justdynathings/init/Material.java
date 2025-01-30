2b6f3c7b2251af8aad6733ebb2205aa38ea2bc71
DevDyna <andrearisorti2003@gmail.com>
2025-01-30 01:01:30 +0100

    Update Material.java

--- src/main/java/com/devdyna/justdynathings/init/Material.java
+++ src/main/java/com/devdyna/justdynathings/init/Material.java
@@ -1,6 +1,7 @@
 package com.devdyna.justdynathings.init;
 
 import com.devdyna.justdynathings.Main;
+import com.devdyna.justdynathings.init.builder.MetalBlock;
 import com.devdyna.justdynathings.init.builder.PhaseBox;
 import com.devdyna.justdynathings.init.builder.reforger.ReforgerBE;
 import com.devdyna.justdynathings.init.builder.reforger.ReforgerBlock;
@@ -69,7 +70,10 @@ public class Material {
         public static final DeferredHolder<Block, PhaseBox> PHASEBOX = zBLK.register("phase_box",
                         () -> new PhaseBox());
 
-        public static final DeferredHolder<Block, Block> RAW_CHAOTIC_BLOCK = zBLK.register("raw_chaotic_ore",
+        public static final DeferredHolder<Block, MetalBlock> METAL_BLOCK = zBLK.register("phase_box",
+                        () -> new MetalBlock());
+
+        public static final DeferredHolder<Block, RawOre> RAW_CHAOTIC_BLOCK = zBLK.register("raw_chaotic_ore",
                         () -> new RawOre(SoundType.AMETHYST, 2.0f, 2.0f));
 
         public static final DeferredHolder<Block, ReforgerBlock> REFORGER_BLOCK = zBLK
@@ -83,11 +87,14 @@ public class Material {
         public static final DeferredHolder<Item, BlockItem> GooT5_ITEM = zITM.register("complex_goo",
                         () -> new GooBlockItem(GooT5_BLOCK.get()));
 
-                        public static final DeferredHolder<Item, Item> RAW_CHAOTIC_ITEM = zITM
+        public static final DeferredHolder<Item, Item> RAW_CHAOTIC_ITEM = zITM
                         .registerSimpleItem("chaotic_dust");
 
-        public static final DeferredHolder<Item, BlockItem> ANTIBLOCK_ITEM = zITM
+        public static final DeferredHolder<Item, BlockItem> PHASEBOX_ITEM = zITM
                         .registerSimpleBlockItem(PHASEBOX);
+
+        public static final DeferredHolder<Item, BlockItem> METALBLOCK_ITEM = zITM
+                        .registerSimpleBlockItem(METAL_BLOCK);
 
         public static final DeferredHolder<Item, BlockItem> REFORGER_ITEM = zITM
                         .register("reforger", () -> new BlockItem(REFORGER_BLOCK.get(), new Item.Properties()));

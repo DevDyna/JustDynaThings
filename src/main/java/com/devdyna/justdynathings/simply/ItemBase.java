005c447543067114960c8a1ab6c5347bb84856db
DevDyna <andrearisorti2003@gmail.com>
2025-02-01 20:15:06 +0100

    Delete ItemBase.java

--- src/main/java/com/devdyna/justdynathings/simply/ItemBase.java
+++
@@ -1,9 +0,0 @@
-package com.devdyna.justdynathings.simply;
-
-import net.minecraft.world.item.Item;
-
-public class ItemBase extends Item {
-    public ItemBase() {
-        super(new Item.Properties());
-    }
-}

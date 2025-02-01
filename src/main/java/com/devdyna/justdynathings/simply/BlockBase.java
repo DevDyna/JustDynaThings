1e17ce4c18a6de50f344be4e25ca6a39af68ee7c
DevDyna <andrearisorti2003@gmail.com>
2025-02-01 20:15:32 +0100

    Delete BlockBase.java

--- src/main/java/com/devdyna/justdynathings/simply/BlockBase.java
+++
@@ -1,9 +0,0 @@
-package com.devdyna.justdynathings.simply;
-
-import net.minecraft.world.level.block.Block;
-
-public class BlockBase extends Block {
-    public BlockBase() {
-        super(Properties.of());
-    }
-}

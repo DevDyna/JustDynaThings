961fc7889e3da5f89e152cc741db678b67cfbd6f
DevDyna <andrearisorti2003@gmail.com>
2025-02-01 20:15:38 +0100

    Create ItemBase.java

---
+++ src/main/java/com/devdyna/justdynathings/init/builder/simply/ItemBase.java
@@ -0,0 +1,9 @@
+package com.devdyna.justdynathings.init.builder.simply;
+
+import net.minecraft.world.item.Item;
+
+public class ItemBase extends Item {
+    public ItemBase() {
+        super(new Item.Properties());
+    }
+}

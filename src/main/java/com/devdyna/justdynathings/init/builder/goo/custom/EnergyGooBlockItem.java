7fd376cb16ed2913198c8c40ba577d911bc4d482
DevDyna <andrearisorti2003@gmail.com>
2025-02-02 00:11:51 +0100

    Create EnergyGooBlockItem.java

---
+++ src/main/java/com/devdyna/justdynathings/init/builder/goo/custom/EnergyGooBlockItem.java
@@ -0,0 +1,11 @@
+package com.devdyna.justdynathings.init.builder.goo.custom;
+
+import com.direwolf20.justdirethings.common.blocks.gooblocks.GooBlock_Item;
+
+import net.minecraft.world.level.block.Block;
+
+public class EnergyGooBlockItem extends GooBlock_Item {
+    public EnergyGooBlockItem(Block block) {
+        super(block, new Properties());
+    }
+}

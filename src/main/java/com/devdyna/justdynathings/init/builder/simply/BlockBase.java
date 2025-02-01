5613e5b513a4bf8b8623f07d36e3259284d411f0
DevDyna <andrearisorti2003@gmail.com>
2025-02-01 20:15:48 +0100

    Create BlockBase.java

---
+++ src/main/java/com/devdyna/justdynathings/init/builder/simply/BlockBase.java
@@ -0,0 +1,9 @@
+package com.devdyna.justdynathings.init.builder.simply;
+
+import net.minecraft.world.level.block.Block;
+
+public class BlockBase extends Block {
+    public BlockBase() {
+        super(Properties.of());
+    }
+}

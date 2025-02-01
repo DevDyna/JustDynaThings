9b7842a59246eaab439b3b6d032afd568beb6390
DevDyna <andrearisorti2003@gmail.com>
2025-02-02 00:11:42 +0100

    Update Main.java

--- src/main/java/com/devdyna/justdynathings/Main.java
+++ src/main/java/com/devdyna/justdynathings/Main.java
@@ -1,12 +1,15 @@
 package com.devdyna.justdynathings;
 
-// import com.devdyna.justdynathings.init.Client;
 import com.devdyna.justdynathings.init.Material;
+import com.direwolf20.justdirethings.common.blockentities.basebe.BaseMachineBE;
+import com.direwolf20.justdirethings.common.blockentities.basebe.PoweredMachineBE;
+import com.direwolf20.justdirethings.setup.Registration;
 
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-// import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.capabilities.Capabilities;
+import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 
 @Mod(Main.ID)
 public class Main {
@@ -14,10 +17,18 @@ public class Main {
 
     public Main(IEventBus modEventBus, ModContainer modContainer) {
         Material.register(modEventBus);
-        // NeoForge.EVENT_BUS.register(new BlockClick());
+        modEventBus.addListener(this::regCap);
+    }
 
-        // if (FMLLoader.getDist().isClient()) {
-        // NeoForge.EVENT_BUS.register(Client.class);
-        // }
+    private void regCap(RegisterCapabilitiesEvent event) {
+
+        event.registerBlock(Capabilities.ItemHandler.BLOCK, (level, pos, state, be, side) -> {
+            return be instanceof BaseMachineBE ? be.getData(Registration.MACHINE_HANDLER) : null;
+        }, Material.REFORGER_BLOCK.get());
+
+        event.registerBlock(Capabilities.EnergyStorage.BLOCK, (level, pos, state, be, side) -> {
+            return be instanceof PoweredMachineBE ? be.getData(Registration.ENERGYSTORAGE_MACHINES) : null;
+        }, Material.GooT4_BLOCK_ENERGY.get());
+
     }
 }

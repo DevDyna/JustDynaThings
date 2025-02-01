25017eaafec01492a18016f657fde9be8f19343e
DevDyna <andrearisorti2003@gmail.com>
2025-02-01 20:18:22 +0100

    Create GooBlockItem.java

---
+++ src/main/java/com/devdyna/justdynathings/init/builder/goo/GooBlockItem.java
@@ -0,0 +1,11 @@
+package com.devdyna.justdynathings.init.builder.goo;
+
+import com.direwolf20.justdirethings.common.blocks.gooblocks.GooBlock_Item;
+
+import net.minecraft.world.level.block.Block;
+
+public class GooBlockItem extends GooBlock_Item {
+    public GooBlockItem(Block block) {
+        super(block, new Properties());
+    }
+}

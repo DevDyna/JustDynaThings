dab42e37484eebaab37565c3f6c5b1caba7dda4c
DevDyna <andrearisorti2003@gmail.com>
2025-02-01 20:17:20 +0100

    Update Client.java

--- src/main/java/com/devdyna/justdynathings/init/Client.java
+++ src/main/java/com/devdyna/justdynathings/init/Client.java
@@ -1,8 +1,8 @@
 package com.devdyna.justdynathings.init;
 
 import com.devdyna.justdynathings.Main;
+import com.devdyna.justdynathings.init.builder.goo.GooRender;
 import com.devdyna.justdynathings.init.builder.reforger.ReforgerScreen;
-import com.devdyna.justdynathings.init.builder.simply.goo.GooRender;
 
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;

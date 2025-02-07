a6fea2ae8cbebbf13d8a33fbcc1ee39d65bc48db
DevDyna <andrearisorti2003@gmail.com>
2025-02-08 00:19:43 +0100

    Update Main.java

--- src/main/java/com/devdyna/justdynathings/Main.java
+++ src/main/java/com/devdyna/justdynathings/Main.java
@@ -1,20 +1,18 @@
 package com.devdyna.justdynathings;
 
 import com.devdyna.justdynathings.init.Material;
-import com.devdyna.justdynathings.init.builder.goo.GooRender;
-import com.devdyna.justdynathings.init.builder.reforger.ReforgerScreen;
 import com.direwolf20.justdirethings.common.blockentities.basebe.BaseMachineBE;
 import com.direwolf20.justdirethings.common.blockentities.basebe.PoweredMachineBE;
 import com.direwolf20.justdirethings.setup.Registration;
 
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.neoforge.capabilities.Capabilities;
+import net.neoforged.neoforge.capabilities.Capabilities.EnergyStorage;
+import net.neoforged.neoforge.capabilities.Capabilities.ItemHandler;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
-import net.neoforged.neoforge.client.event.EntityRenderersEvent;
-import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 import com.mojang.logging.LogUtils;
 import org.slf4j.Logger;
 
@@ -30,22 +28,16 @@ public class Main {
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
+    @SubscribeEvent
     private void regCap(RegisterCapabilitiesEvent event) {
-        event.registerBlock(Capabilities.ItemHandler.BLOCK, (level, pos, state, be, side) -> {
+        event.registerBlock(ItemHandler.BLOCK, (level, pos, state, be, side) -> {
             return be instanceof BaseMachineBE ? be.getData(Registration.MACHINE_HANDLER) : null;
         }, Material.REFORGER_BLOCK.get());
 
-        event.registerBlock(Capabilities.EnergyStorage.BLOCK, (level, pos, state, be, side) -> {
+        event.registerBlock(EnergyStorage.BLOCK, (level, pos, state, be, side) -> {
             return be instanceof PoweredMachineBE ? be.getData(Registration.ENERGYSTORAGE_MACHINES) : null;
         }, Material.GooT4_ENERGY_BLOCK.get());
 
     }
 
-    public void registerScreens(RegisterMenuScreensEvent event) {
-        event.register(Material.GEN_GUI.get(), ReforgerScreen::new);
-    }
-
-    public void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
-        event.registerBlockEntityRenderer(Material.GOO_BE.get(), GooRender::new);
-    }
 }

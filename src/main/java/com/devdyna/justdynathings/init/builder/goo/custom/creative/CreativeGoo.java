ec4ae2d5c92d8a027a5001eb7267f52a2d798ad4
DevDyna <andrearisorti2003@gmail.com>
2025-02-08 00:19:53 +0100

    Update CreativeGoo.java

--- src/main/java/com/devdyna/justdynathings/init/builder/goo/custom/creative/CreativeGoo.java
+++ src/main/java/com/devdyna/justdynathings/init/builder/goo/custom/creative/CreativeGoo.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.annotation.Nullable;
 
+import com.devdyna.justdynathings.Constants;
 import com.devdyna.justdynathings.Main;
 import com.devdyna.justdynathings.init.Material;
 import com.devdyna.justdynathings.init.builder.goo.GooBE;
@@ -19,10 +20,12 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Item.TooltipContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 
 @SuppressWarnings("unused")
 public class CreativeGoo extends GooBlock_Base implements EntityBlock {
@@ -30,10 +33,12 @@ public class CreativeGoo extends GooBlock_Base implements EntityBlock {
    private int tier;
    private int reducer;
 
-   public CreativeGoo(String tipname,  int tier, int reducer) {
+   public CreativeGoo(String tipname, int tier, int reducer) {
       this.tipname = tipname;
       this.tier = tier;
       this.reducer = reducer;
+      this.registerDefaultState(stateDefinition.any()
+            .setValue(ALIVE, true));   //force to alive
    }
 
    @SuppressWarnings("null")
@@ -51,9 +56,9 @@ public class CreativeGoo extends GooBlock_Base implements EntityBlock {
    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents,
          TooltipFlag tooltipFlag) {
       if (Screen.hasControlDown()) {
-         tooltipComponents.add(Component.translatable(Main.ID + "." + tipname + ".on"));
+         tooltipComponents.add(Component.translatable(Main.ID + "." + tipname + "."+Constants.ToolTip.On.id));
       } else {
-         tooltipComponents.add(Component.translatable(Main.ID + ".off"));
+         tooltipComponents.add(Component.translatable(Main.ID + "."+Constants.ToolTip.Off.id));
       }
       super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
    }

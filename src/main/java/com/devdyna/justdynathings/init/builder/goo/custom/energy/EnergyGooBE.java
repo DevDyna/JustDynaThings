f83720de64b642f77e6a4b3f94a2ef9c9b346741
DevDyna <andrearisorti2003@gmail.com>
2025-02-07 20:12:00 +0100

    Update EnergyGooBE.java

--- src/main/java/com/devdyna/justdynathings/init/builder/goo/custom/energy/EnergyGooBE.java
+++ src/main/java/com/devdyna/justdynathings/init/builder/goo/custom/energy/EnergyGooBE.java
@@ -10,14 +10,18 @@ public class EnergyGooBE extends FEGoo {
     private int tier;
     private int reducer;
 
-    public EnergyGooBE(BlockPos pos, BlockState state, int tier, int reducer,int cost) {
-        super(Material.ENERGY_GOO_BE.get(), pos, state,cost);
+    /**
+     * @param cost FE / block converted
+     * @param maxsize Max energy stored
+     */
+    public EnergyGooBE(BlockPos pos, BlockState state, int tier, int reducer,int cost,int maxsize) {
+        super(Material.ENERGY_GOO_BE.get(), pos, state,cost,maxsize);
         this.tier = tier;
         this.reducer = reducer;
     }
 
     public EnergyGooBE(BlockPos pos, BlockState state) {
-        this(pos, state, 0, 1,200);
+        this(pos, state, 0, 1,200,10000);
     }
 
     public int getTier() {

e7fa889fbfd16299928e8328613c1eb467a2d8d9
DevDyna <andrearisorti2003@gmail.com>
2025-02-07 19:46:17 +0100

    Create CreativeGoo.java

---
+++ src/main/java/com/devdyna/justdynathings/init/builder/goo/custom/creative/CreativeGoo.java
@@ -0,0 +1,60 @@
+package com.devdyna.justdynathings.init.builder.goo.custom.creative;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import com.devdyna.justdynathings.Main;
+import com.devdyna.justdynathings.init.Material;
+import com.devdyna.justdynathings.init.builder.goo.GooBE;
+import com.direwolf20.justdirethings.common.blockentities.gooblocks.GooBlockBE_Tier4;
+import com.direwolf20.justdirethings.common.blocks.gooblocks.GooBlock_Base;
+import com.direwolf20.justdirethings.datagen.JustDireItemTags;
+
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Item.TooltipContext;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
+@SuppressWarnings("unused")
+public class CreativeGoo extends GooBlock_Base implements EntityBlock {
+   private String tipname;
+   private int tier;
+   private int reducer;
+
+   public CreativeGoo(String tipname,  int tier, int reducer) {
+      this.tipname = tipname;
+      this.tier = tier;
+      this.reducer = reducer;
+   }
+
+   @SuppressWarnings("null")
+   @Nullable
+   public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+      return new CreativeGooBE(pos, state, tier, reducer);
+   }
+
+   protected boolean validRevivalItem(ItemStack itemStack) {
+      return false;
+   }
+
+   @SuppressWarnings("null")
+   @Override
+   public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents,
+         TooltipFlag tooltipFlag) {
+      if (Screen.hasControlDown()) {
+         tooltipComponents.add(Component.translatable(Main.ID + "." + tipname + ".on"));
+      } else {
+         tooltipComponents.add(Component.translatable(Main.ID + ".off"));
+      }
+      super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+   }
+}

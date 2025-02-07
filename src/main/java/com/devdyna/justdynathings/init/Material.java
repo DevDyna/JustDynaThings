cb21ca65d5a57326611251da020ab1f868ea4ff4
DevDyna <andrearisorti2003@gmail.com>
2025-02-08 00:20:10 +0100

    Update Material.java

--- src/main/java/com/devdyna/justdynathings/init/Material.java
+++ src/main/java/com/devdyna/justdynathings/init/Material.java
@@ -1,5 +1,6 @@
 package com.devdyna.justdynathings.init;
 
+import com.devdyna.justdynathings.Constants;
 import com.devdyna.justdynathings.Main;
 import com.devdyna.justdynathings.init.builder.MetalBlock;
 import com.devdyna.justdynathings.init.builder.PhaseBox;
@@ -69,47 +70,57 @@ public class Material {
 
         // -----------------------------------------------------------------------------------------------------------//
 
-        public static final DeferredHolder<Block, Goo> GooT5_BLOCK = zBLK.register("complex_goo",
-                        () -> new Goo("complex", 5, 20, GOO_REVIVE_TIER_5));
+        public static final DeferredHolder<Block, Goo> GooT5_BLOCK = zBLK.register(
+                        Constants.Material.Goo.Complex.id + "_" + Constants.Material.Goo.ID.id,
+                        () -> new Goo(Constants.Material.Goo.Complex.id + "", 5, 20, GOO_REVIVE_TIER_5));
 
-        public static final DeferredHolder<Block, Goo> GooT0_BLOCK = zBLK.register("rotten_goo",
-                        () -> new Goo("rotten", 0, 4, GOO_REVIVE_TIER_0));
+        public static final DeferredHolder<Block, Goo> GooT0_BLOCK = zBLK.register(
+                        Constants.Material.Goo.Rotten.id + "_" + Constants.Material.Goo.ID.id,
+                        () -> new Goo(Constants.Material.Goo.Rotten.id + "", 0, 4, GOO_REVIVE_TIER_0));
 
-        public static final DeferredHolder<Block, EnergyGoo> GooT4_ENERGY_BLOCK = zBLK.register("energized",
-                        () -> new EnergyGoo("energized", 4, 10, 250,
+        public static final DeferredHolder<Block, EnergyGoo> GooT4_ENERGY_BLOCK = zBLK.register(
+                        Constants.Material.Goo.Energized.id + "_" + Constants.Material.Goo.ID.id,
+                        () -> new EnergyGoo(Constants.Material.Goo.Energized.id + "", 4, 10, 250,
                                         10000));
 
-        public static final DeferredHolder<Block, CreativeGoo> GooT5_CREATIVE_BLOCK = zBLK.register("creative",
-                        () -> new CreativeGoo("creative", 5, 10));
+        public static final DeferredHolder<Block, CreativeGoo> GooT5_CREATIVE_BLOCK = zBLK.register(
+                        Constants.Material.Goo.Creative.id + "_" + Constants.Material.Goo.ID.id,
+                        () -> new CreativeGoo(Constants.Material.Goo.Creative.id, 5, 10));
 
-        public static final DeferredHolder<Block, PhaseBox> PHASEBOX = zBLK.register("phase_box",
+        public static final DeferredHolder<Block, PhaseBox> PHASEBOX = zBLK.register(Constants.Material.PhaseBox.id,
                         () -> new PhaseBox());
 
-        public static final DeferredHolder<Block, MetalBlock> METAL_BLOCK = zBLK.register("metal_block",
+        public static final DeferredHolder<Block, MetalBlock> METAL_BLOCK = zBLK.register(
+                        Constants.Material.MetalBlock.id,
                         () -> new MetalBlock());
 
-        public static final DeferredHolder<Block, RawOre> RAW_CHAOTIC_BLOCK = zBLK.register("raw_chaotic_ore",
+        public static final DeferredHolder<Block, RawOre> RAW_CHAOTIC_BLOCK = zBLK.register(
+                        Constants.Material.Chaotic.Block.id,
                         () -> new RawOre(SoundType.AMETHYST, 2.0f, 2.0f));
 
         public static final DeferredHolder<Block, ReforgerBlock> REFORGER_BLOCK = zBLK
-                        .register("reforger", ReforgerBlock::new);
+                        .register(Constants.Material.Reforger.id, ReforgerBlock::new);
 
         // -----------------------------------------------------------------------------------------------------------//
 
-        public static final DeferredHolder<Item, BlockItem> GooT0_ITEM = zITM.register("rotten_goo",
-                        () -> new GooBlockItem(GooT0_BLOCK.get()));
-
-        public static final DeferredHolder<Item, BlockItem> GooT5_ITEM = zITM.register("complex_goo",
+        public static final DeferredHolder<Item, BlockItem> GooT5_ITEM = zITM.register(
+                        Constants.Material.Goo.Complex.id + "_" + Constants.Material.Goo.ID.id,
                         () -> new GooBlockItem(GooT5_BLOCK.get()));
 
-        public static final DeferredHolder<Item, BlockItem> GooT4_ITEM_Energy = zITM.register("energized",
+        public static final DeferredHolder<Item, BlockItem> GooT0_ITEM = zITM.register(
+                        Constants.Material.Goo.Rotten.id + "_" + Constants.Material.Goo.ID.id,
+                        () -> new GooBlockItem(GooT0_BLOCK.get()));
+
+        public static final DeferredHolder<Item, BlockItem> GooT4_ITEM_Energy = zITM.register(
+                        Constants.Material.Goo.Energized.id + "_" + Constants.Material.Goo.ID.id,
                         () -> new GooBlockItem(GooT4_ENERGY_BLOCK.get()));
 
-        public static final DeferredHolder<Item, BlockItem> GooT5_ITEM_Creative = zITM.register("creative",
+        public static final DeferredHolder<Item, BlockItem> GooT5_ITEM_Creative = zITM.register(
+                        Constants.Material.Goo.Creative.id + "_" + Constants.Material.Goo.ID.id,
                         () -> new GooBlockItem(GooT5_CREATIVE_BLOCK.get()));
 
         public static final DeferredHolder<Item, Item> RAW_CHAOTIC_ITEM = zITM
-                        .registerSimpleItem("chaotic_dust");
+                        .registerSimpleItem(Constants.Material.Chaotic.Item.id);
 
         public static final DeferredHolder<Item, BlockItem> PHASEBOX_ITEM = zITM
                         .registerSimpleBlockItem(PHASEBOX);
@@ -118,39 +129,41 @@ public class Material {
                         .registerSimpleBlockItem(METAL_BLOCK);
 
         public static final DeferredHolder<Item, BlockItem> REFORGER_ITEM = zITM
-                        .register("reforger", () -> new BlockItem(REFORGER_BLOCK.get(), new Item.Properties()));
+                        .register(Constants.Material.Reforger.id,
+                                        () -> new BlockItem(REFORGER_BLOCK.get(), new Item.Properties()));
 
         public static final DeferredHolder<Item, BlockItem> RAW_CHAOTIC_BLOCK_ITEM = zITM
                         .registerSimpleBlockItem(RAW_CHAOTIC_BLOCK);
 
         // -----------------------------------------------------------------------------------------------------------//
 
-        public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<GooBE>> GOO_BE = zBE.register("goobe",
+        public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<GooBE>> GOO_BE = zBE.register(
+                        Constants.Material.Goo.ID.id + "_" + Constants.BlockEntity.id,
                         () -> Builder.of(GooBE::new, GooT5_BLOCK.get(), GooT0_BLOCK.get()).build(null));
 
         public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<EnergyGooBE>> ENERGY_GOO_BE = zBE
-                        .register("energy_goobe",
+                        .register(Constants.Material.Goo.Energized.id + "_" + Constants.BlockEntity.id,
                                         () -> Builder.of(EnergyGooBE::new, GooT4_ENERGY_BLOCK.get()).build(null));
 
         public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<CreativeGooBE>> CREATIVE_GOO_BE = zBE
-                        .register("creative_goobe",
+                        .register(Constants.Material.Goo.Creative.id + "_" + Constants.BlockEntity.id,
                                         () -> Builder.of(CreativeGooBE::new, GooT5_CREATIVE_BLOCK.get()).build(null));
 
         public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<ReforgerBE>> REFORGER_BE = zBE
-                        .register("reforger_be",
+                        .register(Constants.Material.Reforger.id + "_" + Constants.BlockEntity.id,
                                         () -> Builder.of(ReforgerBE::new, REFORGER_BLOCK.get()).build(null));
 
         // -----------------------------------------------------------------------------------------------------------//
 
-        public static final DeferredHolder<MenuType<?>, MenuType<ReforgerGUI>> GEN_GUI = zCTNR
-                        .register("gen_c",
+        public static final DeferredHolder<MenuType<?>, MenuType<ReforgerGUI>> REFORGER_GUI = zCTNR
+                        .register(Constants.Material.Reforger.id + "_" + Constants.GUI.id,
                                         () -> IMenuTypeExtension.create(ReforgerGUI::new));
 
         // -----------------------------------------------------------------------------------------------------------//
 
         public static final DeferredHolder<CreativeModeTab, CreativeModeTab> CreativeTab = zCTBS
                         .register(Main.ID, () -> CreativeModeTab.builder()
-                                        .title(Component.translatable(Main.ID + ".tabname"))
+                                        .title(Component.translatable(Main.ID +"."+ Constants.CreativeTab.id))
                                         .withTabsBefore(CreativeModeTabs.COMBAT)
                                         .icon(() -> GooT5_BLOCK.get().asItem().getDefaultInstance())
                                         .displayItems((parameters, output) -> {

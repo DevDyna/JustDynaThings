5d49290e8d7ee08d501f3b634d0650614e63be39
DevDyna <andrearisorti2003@gmail.com>
2025-02-07 19:29:50 +0100

    Update EnergyGooRender.java

--- src/main/java/com/devdyna/justdynathings/init/builder/goo/custom/EnergyGooRender.java
+++ src/main/java/com/devdyna/justdynathings/init/builder/goo/custom/EnergyGooRender.java
@@ -1,10 +1,11 @@
 package com.devdyna.justdynathings.init.builder.goo.custom;
 
+import com.devdyna.justdynathings.init.builder.goo.custom.core.EnergyGooBase;
 import com.direwolf20.justdirethings.client.blockentityrenders.baseber.GooBlockRender_Base;
 
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 
-public class EnergyGooRender extends GooBlockRender_Base<EnergyGooBE_Base> {
+public class EnergyGooRender extends GooBlockRender_Base<EnergyGooBase> {
     public EnergyGooRender(BlockEntityRendererProvider.Context context) {
        super(context);
     }}

4c52b6e3724ecd277f251876fd0aa1d083fe4cf2
DevDyna <andrearisorti2003@gmail.com>
2025-02-07 19:29:47 +0100

    Update EnergyGooBE.java

--- src/main/java/com/devdyna/justdynathings/init/builder/goo/custom/EnergyGooBE.java
+++ src/main/java/com/devdyna/justdynathings/init/builder/goo/custom/EnergyGooBE.java
@@ -1,31 +1,31 @@
 package com.devdyna.justdynathings.init.builder.goo.custom;
 
-
-import com.devdyna.justdynathings.init.Material;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
+import com.devdyna.justdynathings.init.Material;
+import com.devdyna.justdynathings.init.builder.goo.custom.core.EnergyGooBase;
 
-public class EnergyGooBE extends EnergyGooBE_Base {
+public class EnergyGooBE extends EnergyGooBase {
 
-   private int tier;
-   private int reducer;
+    private int tier;
+    private int reducer;
 
-   
-      public EnergyGooBE(BlockPos pos, BlockState state , int tier,int reducer) {
-          super(Material.ENERGY_GOO_BE.get() , pos, state);
-          this.tier = tier;
-          this.reducer = reducer;
-       }
-   
-       public EnergyGooBE(BlockPos pos, BlockState state) {
-        this(pos, state, 0, 1);
-   }
- 
+    public EnergyGooBE(BlockPos pos, BlockState state, int tier, int reducer,int cost) {
+        super(Material.ENERGY_GOO_BE.get(), pos, state,cost);
+        this.tier = tier;
+        this.reducer = reducer;
+    }
+
+    public EnergyGooBE(BlockPos pos, BlockState state) {
+        this(pos, state, 0, 1,200);
+    }
+
     public int getTier() {
-       return tier;
+        return tier;
     }
- 
+
     public int counterReducer() {
-       return reducer;
+        return reducer;
     }
- }
+
+}

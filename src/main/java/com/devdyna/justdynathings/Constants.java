22e4c67a8cefcae4ab6c522006ad04c600f0495e
DevDyna <andrearisorti2003@gmail.com>
2025-02-08 15:55:00 +0100

    Update Constants.java

--- src/main/java/com/devdyna/justdynathings/Constants.java
+++ src/main/java/com/devdyna/justdynathings/Constants.java
@@ -4,7 +4,7 @@ public enum Constants {
 
     Item("item"),
     Block("block"),
-    BlockEntity(Block.id+"_entity"),
+    BlockEntity(Block.id + "_entity"),
     GUI("gui"),
     CreativeTab("creative_tab"),
     ;
@@ -23,19 +23,25 @@ public enum Constants {
         ;
 
         public final String id;
+
         Material(String id) {
             this.id = id;
         }
 
-        public enum Chaotic {
+        public enum Ore {
 
-            Name("chaotic"),
-            Item(Name.id + "_" + "gem"),
-            Block("raw_" + Name.id + "_ore"),
+            Chaotic("chaotic"),
+            Chaotic_Item(Chaotic.id + "_gem"),
+            Chaotic_Block("raw_" + Chaotic.id + "_ore"),
+
+            Redstonic("redstonic"),
+            Redstonic_Item(Redstonic.id + "_shard"),
+            Redstonic_Block("raw_" + Redstonic.id + "_ore"),
             ;
 
             public final String id;
-            Chaotic(String id) {
+
+            Ore(String id) {
                 this.id = id;
             }
         }
@@ -43,6 +49,10 @@ public enum Constants {
         public enum Goo {
 
             ID("goo"),
+            T1("primogel"),
+            T2("blazebloom"),
+            T3("voidshimmer"),
+            T4("shadowpulse"),
             Rotten("rotten"),
             Complex("complex"),
             Creative("creative"),
@@ -50,6 +60,7 @@ public enum Constants {
             ;
 
             public final String id;
+
             Goo(String id) {
                 this.id = id;
             }
@@ -58,19 +69,28 @@ public enum Constants {
 
     }
 
-
     public enum ToolTip {
-
         On("on"),
         Off("off"),
-        ;
+        Goo("goo");
 
         public final String id;
 
-        ToolTip(String id){
+        ToolTip(String id) {
             this.id = id;
         }
     }
 
+    public enum FEGoo {
+        Cost(250),
+        Capacity(10000),
+        ;
+
+        public final int value;
+
+        FEGoo(int value) {
+            this.value = value;
+        }
+    }
 
 }

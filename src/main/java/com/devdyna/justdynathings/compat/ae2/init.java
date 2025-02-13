1c5056f8f9fe9ed76abb952d18b80d0973745c41
DevDyna <andrearisorti2003@gmail.com>
2025-02-13 21:22:29 +0100

    Update init.java

--- src/main/java/com/devdyna/justdynathings/compat/ae2/init.java
+++ src/main/java/com/devdyna/justdynathings/compat/ae2/init.java
@@ -3,11 +3,6 @@ package com.devdyna.justdynathings.compat.ae2;
 import com.devdyna.justdynathings.Constants;
 import com.devdyna.justdynathings.Main;
 import com.devdyna.justdynathings.common.registry.builder.budding.BuddingBlock;
-import com.devdyna.justdynathings.common.registry.builder.budding.DecayBuddingBlock;
-import com.direwolf20.justdirethings.common.blockentities.basebe.FluidMachineBE;
-import com.direwolf20.justdirethings.common.blockentities.basebe.PoweredMachineBE;
-import com.direwolf20.justdirethings.setup.Registration;
-
 import appeng.core.definitions.AEBlocks;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.BlockItem;
@@ -15,9 +10,6 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.capabilities.Capabilities.EnergyStorage;
-import net.neoforged.neoforge.capabilities.Capabilities.FluidHandler;
-import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -36,8 +28,8 @@ public class init {
         public static final DeferredRegister.Items zAE_ITM = DeferredRegister.createItems(Main.ID);
 
         // -----------------------------------------------------------------------------------------------------------//
-        public static DeferredHolder<Block, BuddingBlock> AE2_FLAWLESS = zAE_BLK.register(
-                        Constants.Material.Budding.Flawless.id + Constants.Material.Budding.Certus.id,
+        public static DeferredHolder<Block, BuddingBlock> AE2_POWERED = zAE_BLK.register(
+                        Constants.Material.Budding.Powered.id + "_" + Constants.Material.Budding.Certus.id,
                         () -> new BuddingBlock(
                                         Constants.FEBudding.FECost.value,
                                         Constants.FEBudding.FECapacity.value,
@@ -48,54 +40,8 @@ public class init {
                                         AEBlocks.LARGE_QUARTZ_BUD.block(),
                                         AEBlocks.QUARTZ_CLUSTER.block()));
 
-        public static DeferredHolder<Block, DecayBuddingBlock> AE2_FLAWED = zAE_BLK.register(
-
-                        Constants.Material.Budding.Flawed.id + Constants.Material.Budding.Certus.id,
-                        () -> new DecayBuddingBlock(
-                                        Constants.FEBudding.FECost.value,
-                                        Constants.FEBudding.FECapacity.value,
-                                        Constants.FEBudding.FLCost.value,
-                                        Constants.FEBudding.FLCapacity.value,
-                                        AEBlocks.SMALL_QUARTZ_BUD.block(),
-                                        AEBlocks.MEDIUM_QUARTZ_BUD.block(),
-                                        AEBlocks.LARGE_QUARTZ_BUD.block(),
-                                        AEBlocks.QUARTZ_CLUSTER.block()));
         // -----------------------------------------------------------------------------------------------------------//
-        public static DeferredHolder<Item, BlockItem> AE2_FLAWLESS_ITEM = zAE_ITM
-                        .registerSimpleBlockItem(AE2_FLAWLESS);
-        public static DeferredHolder<Item, BlockItem> AE2_FLAWED_ITEM = zAE_ITM
-                        .registerSimpleBlockItem(AE2_FLAWED);
+        public static DeferredHolder<Item, BlockItem> AE2_POWERED_ITEM = zAE_ITM
+                        .registerSimpleBlockItem(AE2_POWERED);
         // -----------------------------------------------------------------------------------------------------------//
-        // public static DeferredHolder<BlockEntityType<?>, BlockEntityType<BuddingBE>>
-        // AE2_FLAWLESS_BE = zAE_BE
-        // .register(
-        // Constants.Material.Budding.Flawless.id
-        // + Constants.Material.Budding.Certus.id + "_"
-        // + Constants.BlockEntity.id,
-        // () -> Builder.of(BuddingBE::new, AE2_FLAWLESS.get())
-        // .build(null));
-
-        // public static DeferredHolder<BlockEntityType<?>,
-        // BlockEntityType<DecayBuddingBE>> AE2_FLAWED_BE = zAE_BE
-        // .register(
-        // Constants.Material.Budding.Flawed.id
-        // + Constants.Material.Budding.Certus.id + "_"
-        // + Constants.BlockEntity.id,
-        // () -> Builder.of(DecayBuddingBE::new, AE2_FLAWED.get())
-        // .build(null));
-
-        // -----------------------------------------------------------------------------------------------------------//
-        public static void regCap(RegisterCapabilitiesEvent event) {
-                event.registerBlock(EnergyStorage.BLOCK, (level, pos, state, be,
-                                side) -> be instanceof PoweredMachineBE
-                                                ? be.getData(Registration.ENERGYSTORAGE_MACHINES)
-                                                : null,
-                                AE2_FLAWED.get(), AE2_FLAWLESS.get());
-
-                event.registerBlock(FluidHandler.BLOCK,
-                                (level, pos, state, be, side) -> be instanceof FluidMachineBE
-                                                ? be.getData(Registration.PARADOX_FLUID_HANDLER)
-                                                : null,
-                                AE2_FLAWED.get(), AE2_FLAWLESS.get());
-        }
 }

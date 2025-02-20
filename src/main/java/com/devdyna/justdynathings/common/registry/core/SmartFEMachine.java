c804a306153d2cc848c90d331579dd0340494c9a
DevDyna <andrearisorti2003@gmail.com>
2025-02-21 00:37:43 +0100

    Create SmartFEMachine.java

---
+++ src/main/java/com/devdyna/justdynathings/common/registry/core/SmartFEMachine.java
@@ -0,0 +1,16 @@
+package com.devdyna.justdynathings.common.registry.core;
+
+import com.direwolf20.justdirethings.common.blockentities.basebe.PoweredMachineBE;
+
+public interface SmartFEMachine extends PoweredMachineBE {
+
+    default void extractFEWhenPossible() {
+        extractEnergy(
+                getEnergyStored() <= getStandardEnergyCost() ? getEnergyStored() : getStandardEnergyCost(),
+                false);
+    }
+
+    default boolean validEnergy() {
+        return getEnergyStored() > getStandardEnergyCost();
+    }
+}

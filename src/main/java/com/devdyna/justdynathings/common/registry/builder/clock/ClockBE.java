c042adf8568abea0b39fca08049a743b9f1634b3
DevDyna <andrearisorti2003@gmail.com>
2025-02-27 23:58:39 +0100

    Update ClockBE.java

--- src/main/java/com/devdyna/justdynathings/common/registry/builder/clock/ClockBE.java
+++ src/main/java/com/devdyna/justdynathings/common/registry/builder/clock/ClockBE.java
@@ -1,14 +1,12 @@
 package com.devdyna.justdynathings.common.registry.builder.clock;
 
 import com.devdyna.justdynathings.common.registry.Material;
-import com.devdyna.justdynathings.utils.DirectionUtil;
+import com.devdyna.justdynathings.utils.Actions;
 import com.direwolf20.justdirethings.common.blockentities.basebe.BaseMachineBE;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import static com.devdyna.justdynathings.common.registry.builder.clock.ClockBlock.*;
 
-@SuppressWarnings("null")
 public class ClockBE extends BaseMachineBE {
 
     public ClockBE(BlockEntityType<?> type, BlockPos pos, BlockState blockState) {
@@ -29,22 +27,7 @@ public class ClockBE extends BaseMachineBE {
 
         if (i >= tickSpeed) {
             i = 0;
-            level.setBlockAndUpdate(getBlockPos(),
-                    getBlockState()
-                            .setValue(ACTIVE,
-                                    !getBlockState().getValue(ACTIVE))
-                            .setValue(DirectionUtil.face[0],
-                                    getBlockState().getValue(DirectionUtil.face[0]))
-                            .setValue(DirectionUtil.face[1],
-                                    getBlockState().getValue(DirectionUtil.face[1]))
-                            .setValue(DirectionUtil.face[2],
-                                    getBlockState().getValue(DirectionUtil.face[2]))
-                            .setValue(DirectionUtil.face[3],
-                                    getBlockState().getValue(DirectionUtil.face[3]))
-                            .setValue(DirectionUtil.face[4],
-                                    getBlockState().getValue(DirectionUtil.face[4]))
-                            .setValue(DirectionUtil.face[5],
-                                    getBlockState().getValue(DirectionUtil.face[5])));
+            Actions.clockUpdate(getBlockPos(), level, getBlockState());
         }
 
     }

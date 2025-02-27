d2da275f2f615d449b2e444c37c87650327c4443
DevDyna <andrearisorti2003@gmail.com>
2025-02-27 23:58:55 +0100

    Update SmartFEMachine.java

--- src/main/java/com/devdyna/justdynathings/common/registry/core/interfaces/be/SmartFEMachine.java
+++ src/main/java/com/devdyna/justdynathings/common/registry/core/interfaces/be/SmartFEMachine.java
@@ -1,16 +1,26 @@
 package com.devdyna.justdynathings.common.registry.core.interfaces.be;
 
+import com.devdyna.justdynathings.utils.LevelUtil;
 import com.direwolf20.justdirethings.common.blockentities.basebe.PoweredMachineBE;
+
+import net.minecraft.world.level.Level;
 
 public interface SmartFEMachine extends PoweredMachineBE {
 
+    int getMaxEnergy();
+
     default void extractFEWhenPossible() {
-        extractEnergy(
-                getEnergyStored() <= getStandardEnergyCost() ? getEnergyStored() : getStandardEnergyCost(),
-                false);
-    }
+            extractEnergy(
+                    getEnergyStored() <= getStandardEnergyCost() ? getEnergyStored() : getStandardEnergyCost(),
+                    false);
+        }
 
     default boolean validEnergy() {
         return getEnergyStored() > getStandardEnergyCost();
+    }
+
+    default void extractFEChance(int chance,Level level){
+        if(LevelUtil.chance(chance, level))
+        extractFEWhenPossible();
     }
 }

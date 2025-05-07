b3cfb69e5ba4067ab56c931dee9c760a5f12a95b
DevDyna <andrearisorti2003@gmail.com>
2025-05-07 22:45:49 +0200

    fixed state not triggered and redstone mode

--- src/main/java/com/devdyna/justdynathings/registry/builders/generators/solar/SolarBE.java
+++ src/main/java/com/devdyna/justdynathings/registry/builders/generators/solar/SolarBE.java
@@ -4,20 +4,23 @@ import com.devdyna.justdynathings.registry.interfaces.be.EnergyGenerator;
 import com.devdyna.justdynathings.registry.types.zBlockEntities;
 import com.devdyna.justdynathings.registry.types.zProperties;
 import com.devdyna.justdynathings.utils.Actions;
-import com.devdyna.justdynathings.utils.LogUtil;
 import com.direwolf20.justdirethings.common.blockentities.basebe.BaseMachineBE;
 import com.direwolf20.justdirethings.common.blockentities.basebe.PoweredMachineContainerData;
+import com.direwolf20.justdirethings.common.blockentities.basebe.RedstoneControlledBE;
 import com.direwolf20.justdirethings.common.capabilities.MachineEnergyStorage;
 import com.direwolf20.justdirethings.setup.Registration;
+import com.direwolf20.justdirethings.util.interfacehelpers.RedstoneControlData;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 @SuppressWarnings("null")
-public class SolarBE extends BaseMachineBE implements EnergyGenerator {
+public class SolarBE extends BaseMachineBE implements EnergyGenerator, RedstoneControlledBE {
 
+    public RedstoneControlData redstoneControlData = new RedstoneControlData();
     public final PoweredMachineContainerData poweredMachineData = new PoweredMachineContainerData(this);
 
     public SolarBE(BlockEntityType<?> type, BlockPos pos, BlockState blockState) {
@@ -30,18 +33,17 @@ public class SolarBE extends BaseMachineBE implements EnergyGenerator {
 
     @Override
     public void tickServer() {
-        if (getBlockState().getValue(zProperties.ACTIVE).booleanValue()) {
+        updateBlock();
+        if (isActiveRedstone() && getBlockState().getValue(zProperties.ACTIVE).booleanValue()) {
             increaseFEWhenPossible(FErate / 5);
         }
         if (canExtractFE())
-        Actions.providePowerAdjacent(getBlockPos(), level, FErate);
+            Actions.providePowerAdjacent(getBlockPos(), level, FErate);
     }
 
     public void updateBlock() {
         level.setBlockAndUpdate(getBlockPos(), getBlockState().setValue(zProperties.ACTIVE,
-                level.canSeeSkyFromBelowWater(getBlockPos()) && level.isDay()));
-                LogUtil.info(level.isDay()+"");
-                LogUtil.info(level.canSeeSkyFromBelowWater(getBlockPos())+"");
+                level.canSeeSkyFromBelowWater(getBlockPos().above()) && level.isDay()));
     }
 
     @Override
@@ -62,6 +64,16 @@ public class SolarBE extends BaseMachineBE implements EnergyGenerator {
     @Override
     public MachineEnergyStorage getEnergyStorage() {
         return getData(Registration.ENERGYSTORAGE_GENERATORS);
+    }
+
+    @Override
+    public BlockEntity getBlockEntity() {
+        return this;
+    }
+
+    @Override
+    public RedstoneControlData getRedstoneControlData() {
+        return redstoneControlData;
     }
 
 }

4210d569c8b5aa0ca4d4c03d1a4a75c866a02543
DevDyna <andrearisorti2003@gmail.com>
2025-05-11 13:56:21 +0200

    Update DataBlockModelState.java

--- src/main/java/com/devdyna/justdynathings/datagen/client/DataBlockModelState.java
+++ src/main/java/com/devdyna/justdynathings/datagen/client/DataBlockModelState.java
@@ -22,61 +22,64 @@ import net.neoforged.neoforge.common.data.ExistingFileHelper;
 @SuppressWarnings("unused")
 public class DataBlockModelState extends BlockStateProvider {
 
-    public DataBlockModelState(PackOutput o, ExistingFileHelper f) {
-        super(o, Main.ID, f);
-    }
+        public DataBlockModelState(PackOutput o, ExistingFileHelper f) {
+                super(o, Main.ID, f);
+        }
 
-    @Override
-    protected void registerStatesAndModels() {
-        directionalBlock(zBlocks.BLAZING_ANVIL.get(),
-                models().getExistingFile(DataGenUtil.getResource("block/blazing_anvil")));
+        @Override
+        protected void registerStatesAndModels() {
 
-        BaseGooStateModel(zBlocks.CREATIVE_GOO.get());
-        BaseGooStateModel(zBlocks.ENERGIZED_GOO.get());
-        BaseGooStateModel(zBlocks.T1_GOO.get(), JustDireThings.MODID);
-        BaseGooStateModel(zBlocks.T2_GOO.get(), JustDireThings.MODID);
-        BaseGooStateModel(zBlocks.T3_GOO.get(), JustDireThings.MODID);
-        BaseGooStateModel(zBlocks.T4_GOO.get(), JustDireThings.MODID);
-        PhaseBox(zBlocks.PHASEBOX.get());
+                BaseGooStateModel(zBlocks.CREATIVE_GOO.get());
+                BaseGooStateModel(zBlocks.ENERGIZED_GOO.get());
+                BaseGooStateModel(zBlocks.T1_GOO.get());
+                BaseGooStateModel(zBlocks.T2_GOO.get());
+                BaseGooStateModel(zBlocks.T3_GOO.get());
+                BaseGooStateModel(zBlocks.T4_GOO.get());
+                AnvilStateModel(zBlocks.BLAZING_ANVIL.get());
+                PhaseBox(zBlocks.PHASEBOX.get());
 
-    }
+        }
 
-    private void AnvilStateModel(Block b) {
-        getVariantBuilder(b)
-                .forAllStates(state -> {
-                    Direction dir = state.getValue(AnvilBlock.FACING);
-                    return ConfiguredModel.builder()
-                            .modelFile(models()
-                                    .getExistingFile(DataGenUtil.getResource("block/" + DataGenUtil.getName(b))))
-                            .rotationY(dir == Direction.NORTH || dir == Direction.SOUTH ? 90 : 0)
-                            .build();
-                });
-    }
+        private void AnvilStateModel(Block b) {
+                getVariantBuilder(b)
+                                .forAllStates(state -> {
+                                        Direction dir = state.getValue(AnvilBlock.FACING);
+                                        return ConfiguredModel.builder()
+                                                        .modelFile(models()
+                                                                        .getExistingFile(DataGenUtil.getResource(
+                                                                                        "block/" + DataGenUtil
+                                                                                                        .getName(b))))
+                                                        .rotationY(dir == Direction.NORTH || dir == Direction.SOUTH ? 90
+                                                                        : 0)
+                                                        .build();
+                                });
+        }
 
-    private void BaseGooStateModel(Block b, String modname) {
-        getVariantBuilder(b).partialState().with(GooBlock_Base.ALIVE, true).modelForState()
-                .modelFile(models().getExistingFile(DataGenUtil.getResource(modname + ":block/goo/"
-                        + b.getDescriptionId().replace("block." + modname + ".", "") + "/alive")))
-                .addModel().partialState().with(GooBlock_Base.ALIVE, false).modelForState()
-                .modelFile(models().getExistingFile(DataGenUtil.getResource(
-                        modname + ":block/goo/" + b.getDescriptionId().replace("block." + modname + ".", "")
-                                + "/dead")))
-                .addModel();
-    }
+        private void BaseGooStateModel(Block b, String modname) {
+                getVariantBuilder(b).partialState().with(GooBlock_Base.ALIVE, true).modelForState()
+                                .modelFile(models().getExistingFile(DataGenUtil.getResource("block/goo/"
+                                                + b.getDescriptionId().replace("block." + modname + ".", "") + "/alive",
+                                                modname)))
+                                .addModel().partialState().with(GooBlock_Base.ALIVE, false).modelForState()
+                                .modelFile(models().getExistingFile(DataGenUtil.getResource("block/goo/"
+                                                + b.getDescriptionId().replace("block." + modname + ".", "") + "/dead",
+                                                modname)))
+                                .addModel();
+        }
 
-    private void BaseGooStateModel(Block b) {
-        BaseGooStateModel(b, ID);
-    }
+        private void BaseGooStateModel(Block b) {
+                BaseGooStateModel(b, ID);
+        }
 
-    private void PhaseBox(Block b) {
-        getVariantBuilder(b).partialState().with(zProperties.SOLID, true).modelForState()
-                .modelFile(models().getExistingFile(DataGenUtil.getResource(ID + ":block/"
-                        + b.getDescriptionId().replace("block." + ID + ".", "") + "/" + true)))
-                .addModel().partialState().with(zProperties.SOLID, false).modelForState()
-                .modelFile(models().getExistingFile(DataGenUtil.getResource(
-                        ID + ":block/" + b.getDescriptionId().replace("block." + ID + ".", "")
-                                + "/" + false)))
-                .addModel();
-    }
+        private void PhaseBox(Block b) {
+                getVariantBuilder(b).partialState().with(zProperties.SOLID, true).modelForState()
+                                .modelFile(models().getExistingFile(DataGenUtil.getResource("block/"
+                                                + b.getDescriptionId().replace("block." + ID + ".", "") + "/" + true)))
+                                .addModel().partialState().with(zProperties.SOLID, false).modelForState()
+                                .modelFile(models().getExistingFile(DataGenUtil.getResource(
+                                                "block/" + b.getDescriptionId().replace("block." + ID + ".", "")
+                                                                + "/" + false)))
+                                .addModel();
+        }
 
 }

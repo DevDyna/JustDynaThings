ea064c0ea1ca2a209bcb729a7f24637a9bbf9f1a
DevDyna <andrearisorti2003@gmail.com>
2025-05-11 14:00:12 +0200

    Update DataItemModel.java

--- src/main/java/com/devdyna/justdynathings/datagen/client/DataItemModel.java
+++ src/main/java/com/devdyna/justdynathings/datagen/client/DataItemModel.java
@@ -2,21 +2,14 @@ package com.devdyna.justdynathings.datagen.client;
 
 import static com.devdyna.justdynathings.Main.ID;
 
-import java.util.Set;
-
 import com.devdyna.justdynathings.Main;
 import com.devdyna.justdynathings.registry.types.zBlocks;
 import com.devdyna.justdynathings.registry.types.zItems;
 import com.devdyna.justdynathings.utils.DataGenUtil;
 import net.minecraft.data.PackOutput;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.BucketItem;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
-import net.neoforged.neoforge.client.model.generators.loaders.DynamicFluidContainerModelBuilder;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
-import net.neoforged.neoforge.internal.versions.neoforge.NeoForgeVersion;
-import net.neoforged.neoforge.registries.DeferredHolder;
 
 public class DataItemModel extends ItemModelProvider {
 
@@ -24,54 +17,40 @@ public class DataItemModel extends ItemModelProvider {
                 super(o, Main.ID, f);
         }
 
-        // private Set<DeferredHolder<Item, ?>> blacklist = Set.of(zItems.REDSTONIC_GEM);
-
         @Override
         protected void registerModels() {
 
-                zItems.zItem.getEntries()//.stream().filter(item -> !blacklist.contains(item))
+                zItems.zItem.getEntries()
                                 .forEach(item -> DataGenUtil.itemModel(item.get(), this));
 
                 DataGenUtil.itemBlockwithParent(zBlocks.BLAZING_ANVIL.get(), this,
-                                ID + ":block/blazing_anvil");
+                                ID + ":block/" + DataGenUtil.getName(zBlocks.BLAZING_ANVIL.get()));
 
                 DataGenUtil.itemBlockwithParent(zBlocks.FERRITECORE_CLOCK.get(), this,
-                                "block/cube_all", "all", ID + ":block/ferritecore_clock/on");
-
-                DataGenUtil.itemBlockwithParent(zBlocks.CREATIVE_GOO.get(), this,
-                                ID + ":block/goo/creative/dead");
-
-                DataGenUtil.itemBlockwithParent(zBlocks.ENERGIZED_GOO.get(), this,
-                                ID + ":block/goo/energized/dead");
+                                "block/cube_all", "all",
+                                ID + ":block/" + DataGenUtil.getName(zBlocks.FERRITECORE_CLOCK.get()) + "/on");
 
                 DataGenUtil.itemBlockwithParent(zBlocks.PHASEBOX.get(), this,
-                                ID + ":block/phase_box/true");
-
-                // DataGenUtil.itemBlockwithParent(zBlocks.RAW_CHAOTIC.get(), this,
-                //                 ID + ":block/raw_chaotic_ore");
-
-                // DataGenUtil.itemBlockwithParent(zBlocks.RAW_COPRINIUM.get(), this,
-                //                 ID + ":block/raw_coprinium_ore");
-
-                // DataGenUtil.itemBlockwithParent(zBlocks.RAW_REDSTONIC.get(), this,
-                //                 ID + ":block/raw_redstonic_ore");
+                                ID + ":block/" + DataGenUtil.getName(zBlocks.PHASEBOX.get()) + "/true");
 
                 DataGenUtil.itemBlockwithParent(zBlocks.REFORGER.get(), this,
-                                ID + ":block/reforger/off");
+                                ID + ":block/" + DataGenUtil.getName(zBlocks.REFORGER.get()) + "/off");
 
                 DataGenUtil.itemBlockwithParent(zBlocks.REVITALIZER.get(), this,
-                                ID + ":block/revitalizer/x/off");
+                                ID + ":block/" + DataGenUtil.getName(zBlocks.REVITALIZER.get()) + "/x/off");
 
-                // zItems.zBucketItem.getEntries().forEach(b -> withExistingParent(b.getId().getPath(),
-                //                 ResourceLocation.fromNamespaceAndPath(NeoForgeVersion.MOD_ID, "item/bucket"))
-                //                 .customLoader(DynamicFluidContainerModelBuilder::begin)
-                //                 .fluid(((BucketItem) b.get()).content));
+                GooItemModel(zBlocks.ENERGIZED_GOO.get());
+                GooItemModel(zBlocks.CREATIVE_GOO.get());
+                GooItemModel(zBlocks.T1_GOO.get());
+                GooItemModel(zBlocks.T2_GOO.get());
+                GooItemModel(zBlocks.T3_GOO.get());
+                GooItemModel(zBlocks.T4_GOO.get());
 
-                // NYC
-                // withExistingParent(Items.REDSTONIC_GEM.getId().getPath(),
-                // ResourceLocation.fromNamespaceAndPath("minecraft", "item/generated"))
-                // .customLoader(ItemLayerModelBuilder::begin)
-                // .color(0, 0xFF4C3F7C).renderType("justdynathings:item/shard", 0);
+        }
+
+        private void GooItemModel(Block b) {
+                DataGenUtil.itemBlockwithParent(b, this,
+                                ID + ":block/goo/" + DataGenUtil.getName(b) + "/dead");
 
         }
 

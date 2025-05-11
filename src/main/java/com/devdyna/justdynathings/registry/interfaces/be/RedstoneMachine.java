136031478ab6ce44c9ed68528cbc2b91ef595ea5
DevDyna <andrearisorti2003@gmail.com>
2025-05-11 13:10:48 +0200

    Delete RedstoneMachine.java

--- src/main/java/com/devdyna/justdynathings/registry/interfaces/be/RedstoneMachine.java
+++
@@ -1,8 +0,0 @@
-package com.devdyna.justdynathings.registry.interfaces.be;
-
-import com.direwolf20.justdirethings.common.blockentities.basebe.RedstoneControlledBE;
-
-public interface RedstoneMachine extends RedstoneControlledBE{
-    
-    //TODO
-}

5b6fe95a3420eb5ce1017d4235f4063cf1f86699
DevDyna <andrearisorti2003@gmail.com>
2025-05-11 14:10:34 +0200

    fixed crash on update ferritecore clock

--- src/main/java/com/devdyna/justdynathings/registry/builders/ferritecore_clock/ClockBE.java
+++ src/main/java/com/devdyna/justdynathings/registry/builders/ferritecore_clock/ClockBE.java
@@ -5,6 +5,7 @@ import com.devdyna.justdynathings.registry.types.zProperties;
 import com.devdyna.justdynathings.utils.DirectionUtil;
 import com.direwolf20.justdirethings.common.blockentities.basebe.BaseMachineBE;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -39,7 +40,7 @@ public class ClockBE extends BaseMachineBE {
                 .setValue(zProperties.ACTIVE,
                         !getBlockState().getValue(zProperties.ACTIVE));
 
-        for (int i = 0; i < 7; i++)
+        for (int i = 0; i < Direction.values().length; i++)
             state.setValue(DirectionUtil.face[i],
                     getBlockState().getValue(DirectionUtil.face[i]));
 

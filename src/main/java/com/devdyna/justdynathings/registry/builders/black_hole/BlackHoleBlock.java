f3629932f31ffcee53caebbca1e7fe4d400f4efe
DevDyna <andrearisorti2003@gmail.com>
2025-05-12 16:54:06 +0200

    box for blackhole

--- src/main/java/com/devdyna/justdynathings/registry/builders/black_hole/BlackHoleBlock.java
+++ src/main/java/com/devdyna/justdynathings/registry/builders/black_hole/BlackHoleBlock.java
@@ -10,18 +10,33 @@ import com.direwolf20.justdirethings.common.blocks.baseblocks.BaseMachineBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 @SuppressWarnings("null")
 public class BlackHoleBlock extends BaseMachineBlock {
 
     public BlackHoleBlock() {
         super(zProperties.MachineProp);
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState s, BlockGetter l, BlockPos p, CollisionContext c) {
+        VoxelShape shape = Shapes.empty();
+        shape = Shapes.join(shape, Shapes.box(0.125, 0.875, 0.125, 0.875, 1, 0.875), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.125, 0, 0.125, 0.875, 0.6875, 0.875), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.25, 0.6875, 0.25, 0.75, 0.875, 0.75), BooleanOp.OR);
+
+        return shape.optimize();
     }
 
     @Override
@@ -42,7 +57,7 @@ public class BlackHoleBlock extends BaseMachineBlock {
 
     @Override
     public void openMenu(Player p, BlockPos b) {
-        Actions.openMenu(p,(w, i, e) -> new BlackHoleGUI(w, i, b),b);
+        Actions.openMenu(p, (w, i, e) -> new BlackHoleGUI(w, i, b), b);
     }
 
     @Override

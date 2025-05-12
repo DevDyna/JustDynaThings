89da151f0a20101a9b775aa4471890d66f929148
DevDyna <andrearisorti2003@gmail.com>
2025-05-12 21:11:34 +0200

    thermo shenanegans
    bounding box rotation

--- src/main/java/com/devdyna/justdynathings/registry/builders/generators/thermo/ThermoBlock.java
+++ src/main/java/com/devdyna/justdynathings/registry/builders/generators/thermo/ThermoBlock.java
@@ -5,16 +5,21 @@ import javax.annotation.Nullable;
 import com.devdyna.justdynathings.client.builder.thermoGen.ThermoGUI;
 import com.devdyna.justdynathings.registry.types.zProperties;
 import com.devdyna.justdynathings.utils.Actions;
+import com.devdyna.justdynathings.utils.ShapeUtil;
 import com.direwolf20.justdirethings.common.blocks.baseblocks.BaseMachineBlock;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 @SuppressWarnings("null")
 public class ThermoBlock extends BaseMachineBlock {
@@ -55,6 +60,36 @@ public class ThermoBlock extends BaseMachineBlock {
     @Override
     public boolean isValidBE(BlockEntity b) {
         return b instanceof ThermoBE;
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState s, BlockGetter l, BlockPos p, CollisionContext c) {
+        VoxelShape shape = Shapes.empty();
+
+        switch (s.getValue(BlockStateProperties.FACING)) {
+            case NORTH:
+                shape = ShapeUtil.ThermoGen.north;
+                break;
+            case SOUTH:
+                shape = ShapeUtil.ThermoGen.south;
+                break;
+            case EAST:
+                shape = ShapeUtil.ThermoGen.east;
+                break;
+            case WEST:
+                shape = ShapeUtil.ThermoGen.west;
+                break;
+            case UP:
+                shape = ShapeUtil.ThermoGen.up;
+                break;
+            case DOWN:
+                shape = ShapeUtil.ThermoGen.down;
+                break;
+            default:
+                break;
+        }
+
+        return shape.optimize();
     }
 
 }

c6786892ae0bbcabc7ed3d6f0ad942b3baf115eb
DevDyna <andrearisorti2003@gmail.com>
2025-05-19 22:50:18 +0200

    eclipse alloy solar logic

--- src/main/java/com/devdyna/justdynathings/registry/builders/solar/eclipsealloy/EclipseAlloySolarBE.java
+++ src/main/java/com/devdyna/justdynathings/registry/builders/solar/eclipsealloy/EclipseAlloySolarBE.java
@@ -7,6 +7,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
+@SuppressWarnings("null")
 public class EclipseAlloySolarBE extends SolarBaseBE {
 
     public EclipseAlloySolarBE(BlockEntityType<?> type, BlockPos pos, BlockState blockState) {
@@ -24,7 +25,17 @@ public class EclipseAlloySolarBE extends SolarBaseBE {
 
     @Override
     public int FErate() {
-        return 640;
+        float multipler = 2.0f;
+        int min = level.getMinBuildHeight();
+        int max = level.getMaxBuildHeight();
+        int middle = (Math.abs(min) + max) == 0 ? 10 : (Math.abs(min) + max) / 2;
+
+        int value = getBlockPos().getY();
+
+        if (value != min && value != max)
+            multipler *= (value - middle == 0 ? 1 : Math.abs(value - middle)) / middle;
+
+        return (int) (640 * multipler);
     }
 
     @Override

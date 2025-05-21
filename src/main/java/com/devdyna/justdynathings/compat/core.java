0456b6ec5690ca3985eefaf30f2459c5bba4935f
DevDyna <andrearisorti2003@gmail.com>
2025-05-21 18:00:46 +0200

    Update core.java

--- src/main/java/com/devdyna/justdynathings/compat/core.java
+++ src/main/java/com/devdyna/justdynathings/compat/core.java
@@ -22,16 +22,15 @@ public class core {
         }
 
         private static void ae2_compat(IEventBus bus) {
-                LogUtil.info("AppliedEnergistics2" + (Constants.ModAddonCheck.AppliedEnergistics2 ? " found"
-                                : " not found"));
+                LogUtil.info("AppliedEnergistics2"
+                                + (Constants.ModAddonCheck.AppliedEnergistics2 ? " found " : " not found "));
                 if (Constants.ModAddonCheck.AppliedEnergistics2) {
                         com.devdyna.justdynathings.compat.ae2.init.register(bus);
                 }
         }
 
         private static void extendedae_compat(IEventBus bus) {
-                LogUtil.info("ExtendedAE" + (Constants.ModAddonCheck.ExtendedAE ? " found"
-                                : " not found"));
+                LogUtil.info("ExtendedAE" + (Constants.ModAddonCheck.ExtendedAE ? " found " : " not found "));
                 if (Constants.ModAddonCheck.ExtendedAE) {
                         com.devdyna.justdynathings.compat.extendedae.init.register(bus);
                 }
@@ -39,8 +38,7 @@ public class core {
 
         private static void phasorite_compat(IEventBus bus) {
                 LogUtil.info("PhasoriteNetworks"
-                                + (Constants.ModAddonCheck.PhasoriteNetworks ? " found"
-                                                : " not found"));
+                                + (Constants.ModAddonCheck.PhasoriteNetworks ? " found " : " not found "));
                 if (Constants.ModAddonCheck.PhasoriteNetworks) {
                         com.devdyna.justdynathings.compat.phasorite.init.register(bus);
                 }
@@ -49,8 +47,7 @@ public class core {
         private static void createGuide() {
 
                 LogUtil.info("GuideMe"
-                                + (Constants.ModAddonCheck.GuideMe ? " found"
-                                                : " not found"));
+                                + (Constants.ModAddonCheck.GuideMe ? " found" : " not found"));
                 if (Constants.ModAddonCheck.GuideMe)
                         Guide.builder(ResourceLocation.parse(Main.ID + ":guide")).build();
 

7185e9d874f0d550982a8cd9dcd2c90b4b129e19
DevDyna <andrearisorti2003@gmail.com>
2025-06-06 15:50:40 +0200

    unified DataLoot with BlockDrop

--- src/main/java/com/devdyna/justdynathings/datagen/Controller.java
+++ src/main/java/com/devdyna/justdynathings/datagen/Controller.java
@@ -1,14 +1,19 @@
 package com.devdyna.justdynathings.datagen;
 
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 import com.devdyna.justdynathings.Main;
 import com.devdyna.justdynathings.datagen.client.*;
 import com.devdyna.justdynathings.datagen.server.*;
+
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.DataProvider;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
@@ -33,7 +38,7 @@ public class Controller {
         DataBlockTag blocktag = new DataBlockTag(po, pr, f);
         providerGen(e, g, blocktag);
         providerGen(e, g, new DataItemTag(po, pr, blocktag.contentsGetter()));
-        providerGen(e, g, new DataLoot(po, pr));
+        providerGen(e, g, new LootTableProvider(po, Set.of(), List.of(new LootTableProvider.SubProviderEntry(DataLoot::new, LootContextParamSets.BLOCK)), pr));
         providerGen(e, g, new DataRecipe(po, pr));
         providerGen(e, g, new DataMaps(po, pr));
         providerGen(e, g, new DataFluidTag(po, pr, f));
--- src/main/java/com/devdyna/justdynathings/datagen/server/DataLoot.java
+++ src/main/java/com/devdyna/justdynathings/datagen/server/DataLoot.java
@@ -1,24 +1,60 @@
 package com.devdyna.justdynathings.datagen.server;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.CompletableFuture;
 
-import com.devdyna.justdynathings.datagen.server.tables.BlockDrop;
+import com.devdyna.justdynathings.registry.types.zBlocks;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
-import net.minecraft.core.HolderLookup.Provider;
-import net.minecraft.data.PackOutput;
-import net.minecraft.data.loot.LootTableProvider;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+public class DataLoot extends BlockLootSubProvider {
 
-public class DataLoot extends LootTableProvider {
+        public DataLoot(HolderLookup.Provider l) {
+                super(Set.of(), FeatureFlags.DEFAULT_FLAGS, l);
+        }
 
-    public DataLoot(PackOutput o, CompletableFuture<Provider> r) {
-        super(o, Set.of(),
-                List.of(
-                    //list of any loot table entries inside ./server/tables/
-                        new LootTableProvider.SubProviderEntry(BlockDrop::new, LootContextParamSets.BLOCK)),
-                r);
-    }
+        @Override
+        protected Iterable<Block> getKnownBlocks() {
+                List<Block> blocks = new ArrayList<>();
+                blocks.addAll(getList(zBlocks.zBlock));
+                blocks.addAll(getList(zBlocks.zBlockItem));
+                blocks.addAll(getList(zBlocks.zGoo));
+                blocks.addAll(getList(zBlocks.zOres));
+                return blocks;
+        }
+
+        @SuppressWarnings("unchecked")
+        private List<Block> getList(DeferredRegister.Blocks c) {
+                return (List<Block>) c.getEntries().stream().map(DeferredHolder::get).toList();
+        }
+
+        @Override
+        protected void generate() {
+
+                zBlocks.zBlock.getEntries().forEach(b -> dropSelf(b.get()));
+                zBlocks.zBlockItem.getEntries().forEach(b -> dropSelf(b.get()));
+                zBlocks.zGoo.getEntries().forEach(b -> dropSelf(b.get()));
+
+                // oreTable(zBlocks.REDSTONE_ORE.get(), zItems.REDSTONE_FUEL.get(), 3, 5);
+                // oreTable(zBlocks.LAPIS_LAZULI_ORE.get(), zItems.LAPIS_LAZULI_FUEL.get(), 3, 5);
+
+        }
+
+        @SuppressWarnings("unused")
+        private void oreTable(Block b, Item drop, float min, float max) {
+                add(b, createSilkTouchDispatchTable(b,
+                                this.applyExplosionDecay(b, LootItem.lootTableItem(drop)
+                                                .apply(SetItemCountFunction
+                                                                .setCount(UniformGenerator.between(min, max))))));
+        }
 
 }

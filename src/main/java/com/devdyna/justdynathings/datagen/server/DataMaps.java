4f570264e2417d526305c7c65fc8dec12f4448a2
DevDyna <andrearisorti2003@gmail.com>
2025-06-27 22:54:49 +0200

    Update DataMaps.java

--- src/main/java/com/devdyna/justdynathings/datagen/server/DataMaps.java
+++ src/main/java/com/devdyna/justdynathings/datagen/server/DataMaps.java
@@ -2,27 +2,38 @@ package com.devdyna.justdynathings.datagen.server;
 
 import java.util.concurrent.CompletableFuture;
 
+import com.devdyna.justdynathings.datamaps.zDataMaps;
+import com.devdyna.justdynathings.datamaps.records.*;
+import com.devdyna.justdynathings.registry.types.zFluidTags;
+import com.devdyna.justdynathings.registry.types.zItemTags;
+import com.direwolf20.justdirethings.setup.Registration;
+
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.data.PackOutput;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.Fluids;
+import net.neoforged.neoforge.common.conditions.ModLoadedCondition;
 import net.neoforged.neoforge.common.data.DataMapProvider;
+import net.neoforged.neoforge.fluids.FluidStack;
+
 @SuppressWarnings("unused")
 public class DataMaps extends DataMapProvider {
 
-    
     private static int BASE_BURN_RATE = 200; // 1 item
 
     public DataMaps(PackOutput p, CompletableFuture<Provider> l) {
         super(p, l);
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     protected void gather() {
 
-        // builder(NeoForgeDataMaps.FURNACE_FUELS)
-        //         .add(zBlocks.REDSTONE_FUEL.getId(), new FurnaceFuel(BASE_BURN_RATE * 1440), false)
-        //         .add(zBlocks.LAPIS_LAZULI_FUEL.getId(), new FurnaceFuel(BASE_BURN_RATE * 480), false)
-        //         .add(zItems.REDSTONE_FUEL, new FurnaceFuel(BASE_BURN_RATE * 144), false)
-        //         .add(zItems.LAPIS_LAZULI_FUEL, new FurnaceFuel(BASE_BURN_RATE * 48), false);
+        builder(zDataMaps.FERRICORE_REPAIR)
+                .add(Registration.FerricoreIngot.get().builtInRegistryHolder(), new FerricoreRepair(128), false)
+                .add(Items.IRON_INGOT.builtInRegistryHolder(), new FerricoreRepair(64), false);
 
     }
 

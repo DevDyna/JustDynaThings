caf18ec8205d66e63300f7b0cbb48425740de887
DevDyna <andrearisorti2003@gmail.com>
2025-06-27 14:42:40 +0200

    Update zMultiTags.java

--- src/main/java/com/devdyna/justdynathings/registry/types/zMultiTags.java
+++ src/main/java/com/devdyna/justdynathings/registry/types/zMultiTags.java
@@ -11,7 +11,6 @@ public class zMultiTags {
     public static final MultiTag COPPER_BLOCKS = new MultiTag("minecraft", "copper_blocks");
     public static final MultiTag AMETHYST_BLOCKS = new MultiTag("minecraft", "amethyst_blocks");
 
-
     public static final MultiTag T2_SPREAD = new MultiTag(ID, "t2_spread");
     public static final MultiTag T3_SPREAD = new MultiTag(ID, "t3_spread");
     public static final MultiTag T4_SPREAD = new MultiTag(ID, "t4_spread");

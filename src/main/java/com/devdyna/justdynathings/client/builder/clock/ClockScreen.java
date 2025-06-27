60f3b450d1cf8ae59094a319e3e7c7d7d7f7b998
DevDyna <andrearisorti2003@gmail.com>
2025-06-27 21:24:39 +0200

    temp removed NYI buttons

--- src/main/java/com/devdyna/justdynathings/client/builder/clock/ClockScreen.java
+++ src/main/java/com/devdyna/justdynathings/client/builder/clock/ClockScreen.java
@@ -1,13 +1,10 @@
 package com.devdyna.justdynathings.client.builder.clock;
 
-import com.devdyna.justdynathings.client.core.FactoryButtons;
-import com.devdyna.justdynathings.utils.DirectionUtil;
 import com.direwolf20.justdirethings.client.screens.basescreens.BaseMachineScreen;
 import com.direwolf20.justdirethings.client.screens.standardbuttons.ToggleButtonFactory;
 import com.direwolf20.justdirethings.client.screens.widgets.NumberButton;
 import com.direwolf20.justdirethings.common.network.data.TickSpeedPayload;
 
-import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.world.entity.player.Inventory;
@@ -23,7 +20,7 @@ public class ClockScreen extends BaseMachineScreen<ClockGUI> {
         super.init();
     }
 
-    @SuppressWarnings("null")
+//     @SuppressWarnings("null")
     @Override
     public void addTickSpeedButton() {
         this.addRenderableWidget(ToggleButtonFactory.TICKSPEEDBUTTON(
@@ -36,22 +33,22 @@ public class ClockScreen extends BaseMachineScreen<ClockGUI> {
                             new CustomPacketPayload[0]);
                 }));
 
-        for (Direction d : DirectionUtil.ALL)
-            this.addRenderableWidget(
-                    FactoryButtons.SIDE(
-                            this.getGuiLeft() +
-                                    102,
-                            this.topSectionTop
-                                    + 60,
-                            true,
-                            d,
-                            (b) -> {
-                                // need to be payloaded , now it dont work
-                                this.baseMachineBE.getLevel().setBlockAndUpdate(this.baseMachineBE.getBlockPos(),
-                                        this.baseMachineBE.getBlockState()
-                                                .setValue(
-                                                        DirectionUtil.face[DirectionUtil.indexByDir(d)],
-                                                        !b.active));
-                            }));
+        // for (Direction d : DirectionUtil.ALL)
+        //     this.addRenderableWidget(
+        //             FactoryButtons.SIDE(
+        //                     this.getGuiLeft() +
+        //                             102,
+        //                     this.topSectionTop
+        //                             + 60,
+        //                     true,
+        //                     d,
+        //                     (b) -> {
+        //                         // need to be payloaded , now it dont work
+        //                         this.baseMachineBE.getLevel().setBlockAndUpdate(this.baseMachineBE.getBlockPos(),
+        //                                 this.baseMachineBE.getBlockState()
+        //                                         .setValue(
+        //                                                 DirectionUtil.face[DirectionUtil.indexByDir(d)],
+        //                                                 !b.active));
+        //                     }));
     }
 }

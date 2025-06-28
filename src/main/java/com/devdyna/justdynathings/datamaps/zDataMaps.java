65c11dff07dc4cc91e429c2b782f5a72f896cdd3
DevDyna <andrearisorti2003@gmail.com>
2025-06-28 14:51:22 +0200

    fixed unsynched

--- src/main/java/com/devdyna/justdynathings/datamaps/zDataMaps.java
+++ src/main/java/com/devdyna/justdynathings/datamaps/zDataMaps.java
@@ -18,27 +18,27 @@ public class zDataMaps {
         public static final DataMapType<Item, FerricoreItemRepair> FERRICORE_REPAIR = DataMapType.builder(
                         ResourceLocation.fromNamespaceAndPath(ID, "anvils/ferricore_repair"),
                         Registries.ITEM,
-                        FerricoreItemRepair.CODEC).build();
+                        FerricoreItemRepair.CODEC).synced(FerricoreItemRepair.CODEC, false).build();
 
         public static final DataMapType<Block, ThermoBlockHeatSource> THERMO_HEAT_SOURCE = DataMapType.builder(
                         ResourceLocation.fromNamespaceAndPath(ID, "thermo_heat_sources"),
                         Registries.BLOCK,
-                        ThermoBlockHeatSource.CODEC).build();
+                        ThermoBlockHeatSource.CODEC).synced(ThermoBlockHeatSource.CODEC, false).build();
 
         public static final DataMapType<Fluid, ThermoFluidCoolant> THERMO_COOLANT = DataMapType.builder(
                         ResourceLocation.fromNamespaceAndPath(ID, "thermo_coolants"),
                         Registries.FLUID,
-                        ThermoFluidCoolant.CODEC).build();
+                        ThermoFluidCoolant.CODEC).synced(ThermoFluidCoolant.CODEC, false).build();
 
         public static final DataMapType<Fluid, BlazeGoldFluidRepair> BLAZEGOLD_FLUID = DataMapType.builder(
                         ResourceLocation.fromNamespaceAndPath(ID, "anvils/blazegold_repair"),
                         Registries.FLUID,
-                        BlazeGoldFluidRepair.CODEC).build();
+                        BlazeGoldFluidRepair.CODEC).synced(BlazeGoldFluidRepair.CODEC, false).build();
 
         public static final DataMapType<Fluid, EclipseAlloyFluidRepair> ECLIPSEALLOY_FLUID = DataMapType.builder(
                         ResourceLocation.fromNamespaceAndPath(ID, "anvils/eclipsealloy_repair"),
                         Registries.FLUID,
-                        EclipseAlloyFluidRepair.CODEC).build();
+                        EclipseAlloyFluidRepair.CODEC).synced(EclipseAlloyFluidRepair.CODEC, false).build();
 
         @SubscribeEvent
         public static void register(RegisterDataMapTypesEvent event) {

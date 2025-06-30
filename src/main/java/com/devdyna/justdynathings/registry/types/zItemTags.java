ca93c35aaead385019d8ff590b1530f8ba57cc33
DevDyna <andrearisorti2003@gmail.com>
2025-06-30 21:21:27 +0200

    Update zItemTags.java

--- src/main/java/com/devdyna/justdynathings/registry/types/zItemTags.java
+++ src/main/java/com/devdyna/justdynathings/registry/types/zItemTags.java
@@ -9,28 +9,32 @@ import net.neoforged.bus.api.IEventBus;
 
 public class zItemTags {
         public static void register(IEventBus bus) {
-
         }
 
         public static final TagKey<Item> REFORGER_CATALYST = Material.tagItem("reforger_catalyst");
+
         public static final TagKey<Item> CREATIVE_GOO_WRENCHES = Material.tagItem(Constants.Goo.Creative + "_wrenches");
 
         public static final TagKey<Item> UNIVERSAL_WRENCH = Material.tagItem("universal_wrench");
 
         public static final TagKey<Item> FERRICORE_ANVIL_DENY = Material
                         .tagItem(Constants.AnvilType + "s/" + Constants.Tiers.ferricore + "/deny");
-        // public static final TagKey<Item> FERRICORE_ANVIL_REPAIR = Material
-        //                 .tagItem(Constants.AnvilType + "s/" + Constants.Tiers.ferricore + "/catalyst");
+
         public static final TagKey<Item> BLAZEGOLD_ANVIL_DENY = Material
                         .tagItem(Constants.AnvilType + "s/" + Constants.Tiers.blazegold + "/deny");
+
         public static final TagKey<Item> CELESTIGEM_DENY = Material
                         .tagItem(Constants.AnvilType + "s/" + Constants.Tiers.celestigem + "/deny");
+
         public static final TagKey<Item> ECLIPSE_ALLOY_ANVIL_DENY = Material
                         .tagItem(Constants.AnvilType + "s/" + Constants.Tiers.eclipsealloy + "/deny");
 
         public static final TagKey<Item> TIME_BUDDING = Material.tagItem("time_buddings");
+
         public static final TagKey<Item> AE2_COMPAT = Material.tagItem("compat/ae2/buddings");
+
         public static final TagKey<Item> EXT_COMPAT = Material.tagItem("compat/extendedae/buddings");
+
         public static final TagKey<Item> PHA_COMPAT = Material.tagItem("compat/phasoritenetworks/buddings");
 
 }

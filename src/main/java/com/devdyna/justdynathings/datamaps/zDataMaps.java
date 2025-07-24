e978de07db925c79bdb146e2a6b986430271eeac
DevDyna <andrearisorti2003@gmail.com>
2025-07-24 16:10:29 +0200

    Update zDataMaps.java

--- src/main/java/com/devdyna/justdynathings/datamaps/zDataMaps.java
+++ src/main/java/com/devdyna/justdynathings/datamaps/zDataMaps.java
@@ -2,6 +2,7 @@ package com.devdyna.justdynathings.datamaps;
 
 import static com.devdyna.justdynathings.Main.ID;
 
+import com.devdyna.justdynathings.Constants;
 import com.devdyna.justdynathings.datamaps.RecordMap.*;
 
 import net.minecraft.core.registries.Registries;
@@ -16,52 +17,54 @@ import net.neoforged.neoforge.registries.datamaps.RegisterDataMapTypesEvent;
 public class zDataMaps {
 
         public static final DataMapType<Item, FerricoreItemRepair> FERRICORE_REPAIR = DataMapType.builder(
-                        ResourceLocation.fromNamespaceAndPath(ID, "anvils/ferricore_repair"),
+                        ResourceLocation.fromNamespaceAndPath(ID, Constants.DataMaps.Anvils.ferricore_repair),
                         Registries.ITEM,
                         FerricoreItemRepair.CODEC).synced(FerricoreItemRepair.CODEC, false).build();
 
-        public static final DataMapType<Block, ThermoBlockHeatSource> THERMO_HEAT_SOURCE = DataMapType.builder(
-                        ResourceLocation.fromNamespaceAndPath(ID, "thermo_heat_sources"),
-                        Registries.BLOCK,
-                        ThermoBlockHeatSource.CODEC).synced(ThermoBlockHeatSource.CODEC, false).build();
-
-        public static final DataMapType<Fluid, ThermoFluidCoolant> THERMO_COOLANT = DataMapType.builder(
-                        ResourceLocation.fromNamespaceAndPath(ID, "thermo_coolants"),
-                        Registries.FLUID,
-                        ThermoFluidCoolant.CODEC).synced(ThermoFluidCoolant.CODEC, false).build();
-
         public static final DataMapType<Fluid, BlazeGoldFluidRepair> BLAZEGOLD_FLUID = DataMapType.builder(
-                        ResourceLocation.fromNamespaceAndPath(ID, "anvils/blazegold_repair"),
+                        ResourceLocation.fromNamespaceAndPath(ID, Constants.DataMaps.Anvils.blazegold_repair),
                         Registries.FLUID,
                         BlazeGoldFluidRepair.CODEC).synced(BlazeGoldFluidRepair.CODEC, false).build();
 
         public static final DataMapType<Fluid, EclipseAlloyFluidRepair> ECLIPSEALLOY_FLUID = DataMapType.builder(
-                        ResourceLocation.fromNamespaceAndPath(ID, "anvils/eclipsealloy_repair"),
+                        ResourceLocation.fromNamespaceAndPath(ID, Constants.DataMaps.Anvils.eclipsealloy_repair),
                         Registries.FLUID,
                         EclipseAlloyFluidRepair.CODEC).synced(EclipseAlloyFluidRepair.CODEC, false).build();
 
+        public static final DataMapType<Block, ThermoBlockHeatSource> THERMO_HEAT_SOURCE = DataMapType.builder(
+                        ResourceLocation.fromNamespaceAndPath(ID, Constants.DataMaps.Thermo.thermo_heat_sources),
+                        Registries.BLOCK,
+                        ThermoBlockHeatSource.CODEC).synced(ThermoBlockHeatSource.CODEC, false).build();
+
+        public static final DataMapType<Fluid, ThermoFluidCoolant> THERMO_COOLANT = DataMapType.builder(
+                        ResourceLocation.fromNamespaceAndPath(ID, Constants.DataMaps.Thermo.thermo_coolants),
+                        Registries.FLUID,
+                        ThermoFluidCoolant.CODEC).synced(ThermoFluidCoolant.CODEC, false).build();
+
         public static final DataMapType<Item, ReforgerResult.oneToOne> REFORGER_oneToOne = DataMapType.builder(
-                        ResourceLocation.fromNamespaceAndPath(ID, "reforger_conversion/block_to_block"),
+                        ResourceLocation.fromNamespaceAndPath(ID, Constants.DataMaps.Reforger.block_to_block),
                         Registries.ITEM,
                         ReforgerResult.oneToOne.CODEC).synced(ReforgerResult.oneToOne.CODEC, false).build();
 
         public static final DataMapType<Item, ReforgerResult.oneToMany> REFORGER_oneToMany = DataMapType.builder(
-                        ResourceLocation.fromNamespaceAndPath(ID, "reforger_conversion/block_to_tag"),
+                        ResourceLocation.fromNamespaceAndPath(ID, Constants.DataMaps.Reforger.block_to_tag),
                         Registries.ITEM,
                         ReforgerResult.oneToMany.CODEC).synced(ReforgerResult.oneToMany.CODEC, false).build();
 
         public static final DataMapType<Item, ReforgerResult.manyToOne> REFORGER_manyToOne = DataMapType.builder(
-                        ResourceLocation.fromNamespaceAndPath(ID, "reforger_conversion/tag_to_block"),
+                        ResourceLocation.fromNamespaceAndPath(ID, Constants.DataMaps.Reforger.tag_to_block),
                         Registries.ITEM,
                         ReforgerResult.manyToOne.CODEC).synced(ReforgerResult.manyToOne.CODEC, false).build();
 
         @SubscribeEvent
         public static void register(RegisterDataMapTypesEvent event) {
                 event.register(FERRICORE_REPAIR);
-                event.register(THERMO_HEAT_SOURCE);
-                event.register(THERMO_COOLANT);
                 event.register(BLAZEGOLD_FLUID);
                 event.register(ECLIPSEALLOY_FLUID);
+
+                event.register(THERMO_HEAT_SOURCE);
+                event.register(THERMO_COOLANT);
+
                 event.register(REFORGER_oneToOne);
                 event.register(REFORGER_oneToMany);
                 event.register(REFORGER_manyToOne);

89e7f47ff6e89cfbb45cd31e6bfe88d2e3be729d
DevDyna <andrearisorti2003@gmail.com>
2025-07-24 16:10:38 +0200

    Update startup.java

--- src/main/java/com/devdyna/justdynathings/config/startup.java
+++ src/main/java/com/devdyna/justdynathings/config/startup.java
@@ -11,26 +11,26 @@ import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.common.ModConfigSpec;
 
-public class startup {
-    private static final ModConfigSpec.Builder qSTARTUP = new ModConfigSpec.Builder();
+// public class startup {
+//     private static final ModConfigSpec.Builder qSTARTUP = new ModConfigSpec.Builder();
 
-    public static ModConfigSpec.ConfigValue<List<? extends String>> ITEMS;
+//     public static ModConfigSpec.ConfigValue<List<? extends String>> ITEMS;
 
-    public static void register(ModContainer c) {
-        regStartup();
-        c.registerConfig(ModConfig.Type.STARTUP, qSTARTUP.build());
-    }
+//     public static void register(ModContainer c) {
+//         regStartup();
+//         c.registerConfig(ModConfig.Type.STARTUP, qSTARTUP.build());
+//     }
 
-    private static void regStartup() {
+//     private static void regStartup() {
 
-        ITEMS = qSTARTUP
-                .comment("List of all generated Raw ores")
-                .defineListAllowEmpty("items", List.of(DataGenUtil.getName(Blocks.DIAMOND_ORE)), () -> "",
-                        startup::checkItem);
-    }
+//         ITEMS = qSTARTUP
+//                 .comment("List of all generated Raw ores")
+//                 .defineListAllowEmpty("items", List.of(DataGenUtil.getName(Blocks.DIAMOND_ORE)), () -> "",
+//                         startup::checkItem);
+//     }
 
-    private static boolean checkItem(final Object obj) {
-        return obj instanceof String name && BuiltInRegistries.ITEM.containsKey(ResourceLocation.parse(name));
-    }
+//     private static boolean checkItem(final Object obj) {
+//         return obj instanceof String name && BuiltInRegistries.ITEM.containsKey(ResourceLocation.parse(name));
+//     }
 
-}
+// }

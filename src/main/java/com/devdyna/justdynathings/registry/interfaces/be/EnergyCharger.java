3c6f3d600a39a0fc67defe66009f0f564a5f0d25
DevDyna <andrearisorti2003@gmail.com>
2025-07-25 14:00:22 +0200

    Update EnergyCharger.java

--- src/main/java/com/devdyna/justdynathings/registry/interfaces/be/EnergyCharger.java
+++ src/main/java/com/devdyna/justdynathings/registry/interfaces/be/EnergyCharger.java
@@ -1,26 +1,33 @@
 package com.devdyna.justdynathings.registry.interfaces.be;
 
+import com.devdyna.justdynathings.utils.LogUtil;
+import com.direwolf20.justdirethings.setup.Registration;
+
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.energy.IEnergyStorage;
+import net.neoforged.neoforge.items.ItemStackHandler;
 
+@SuppressWarnings("null")
 public interface EnergyCharger {
 
-    default void chargeFEtoItemStack(ItemStack item , IEnergyStorage block){
+    default void chargeFEtoItemStack(Level level, BlockPos pos, ItemStack item, IEnergyStorage block) {
+            if (block.canExtract() && item != null) {
+                var cap = item.getCapability(Capabilities.EnergyStorage.ITEM);
 
-        if (block.canExtract() && item != null ) {
-            var cap = item.getCapability(Capabilities.EnergyStorage.ITEM);
+                if (cap == null)
+                    return;
 
-            if (cap == null)
-                return;
+                if (!cap.canReceive() || !block.canExtract())
+                    return;
 
-            if (!cap.canReceive() || !block.canExtract())
-                return;
+                cap.receiveEnergy(
+                        block.extractEnergy(cap.getMaxEnergyStored() - cap.getEnergyStored(),
+                                false),
+                        false);
 
-            cap.receiveEnergy(
-                    block.extractEnergy(cap.getMaxEnergyStored() - cap.getEnergyStored(),
-                            false),
-                    false);
 
         }
 

2106dd10497433d36fb1b65d4cc8e7f8d89e4557
DevDyna <andrearisorti2003@gmail.com>
2025-11-14 14:14:51 +0100

    reduced default particles and sound event to creative goo

--- src/main/java/com/devdyna/justdynathings/registry/builders/goo/creative/NoDieGooLogic.java
+++ src/main/java/com/devdyna/justdynathings/registry/builders/goo/creative/NoDieGooLogic.java
@@ -1,6 +1,9 @@
 package com.devdyna.justdynathings.registry.builders.goo.creative;
 
+import java.util.Random;
+
 import com.devdyna.justdynathings.config.common;
+import com.direwolf20.justdirethings.client.particles.gooexplodeparticle.GooExplodeParticleData;
 
 // import static com.direwolf20.justdirethings.common.blocks.gooblocks.GooBlock_Base.ALIVE;
 
@@ -9,6 +12,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -37,8 +41,41 @@ public class NoDieGooLogic extends GooBlockBE_Base {
 
         updateSideCounter(direction, -1);
         sidedDurations.put(direction, -1);
-        if(common.GOO_CREATIVE_SOUND_RECIPE.get())
-        level.playSound(null, getBlockPos(), SoundEvents.SCULK_BLOCK_BREAK, SoundSource.BLOCKS, 1.0F, 1.0F);
+        if (common.GOO_CREATIVE_SOUND_RECIPE.get())
+            level.playSound(null, getBlockPos(), SoundEvents.SCULK_BLOCK_BREAK, SoundSource.BLOCKS, 0.25F, 1.0F);
+    }
+
+    /**
+     * A less client-fps expensive version of
+     * <code>GooBlockBE_Base.spawnParticles(Direction side)</code>
+     * <br/>
+     * <br/>
+     * Aprox. 1/8 of particles generated
+     */
+    @SuppressWarnings("null")
+    @Override
+    public void spawnParticles(Direction side) {
+        Random random = new Random();
+        BlockPos startPos = getBlockPos().relative(side);
+        ItemStack itemStack = new ItemStack(getBlockState().getBlock());
+        GooExplodeParticleData data = new GooExplodeParticleData(itemStack);
+
+        for (int d = 0; d < Direction.values().length; ++d) {
+            Direction direction = Direction.values()[d];
+
+            var x = direction.getNormal().getX();
+            var y = direction.getNormal().getY();
+            var z = direction.getNormal().getZ();
+
+            for (int i = 0; i < 12; ++i) {
+                double randomX = 0.5 + 0.6 * (double) x + (x == 0 ? random.nextDouble() - 0.5 : 0.0);
+                double randomY = 0.5 + 0.6 * (double) y + (y == 0 ? random.nextDouble() - 0.5 : 0.0);
+                double randomZ = 0.5 + 0.6 * (double) z + (z == 0 ? random.nextDouble() - 0.5 : 0.0);
+                level.addParticle(data, (double) startPos.getX() + randomX, (double) startPos.getY() + randomY,
+                        (double) startPos.getZ() + randomZ, 0.0, 0.0, 0.0);
+            }
+        }
+
     }
 
 }

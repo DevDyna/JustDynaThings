3e201d58d7e8c85d9bae6d59e880bb713b59ceb7
DevDyna <andrearisorti2003@gmail.com>
2025-11-18 17:01:36 +0100

    testing to optimize echoing buddings

--- src/main/java/com/devdyna/justdynathings/registry/builders/budding/types/amethyst/AmethystBE.java
+++ src/main/java/com/devdyna/justdynathings/registry/builders/budding/types/amethyst/AmethystBE.java
@@ -5,21 +5,16 @@ import com.devdyna.justdynathings.registry.types.zBlockEntities;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class AmethystBE extends BuddingBE {
 
-    public AmethystBE(BlockEntityType<?> type, BlockPos pos, BlockState state) {
-        super(type, pos, state);
+    public AmethystBE(BlockPos pos, BlockState state) {
+        super(zBlockEntities.ECHOING_BUDDING_AMETHYST.get(), pos, state);
         this.smallCluster = Blocks.SMALL_AMETHYST_BUD;
         this.mediumCluster = Blocks.MEDIUM_AMETHYST_BUD;
         this.largeCluster = Blocks.LARGE_AMETHYST_BUD;
         this.finalCluster = Blocks.AMETHYST_CLUSTER;
-    }
-
-    public AmethystBE(BlockPos pos, BlockState state) {
-        this(zBlockEntities.ECHOING_BUDDING_AMETHYST.get(), pos, state);
     }
 
 }

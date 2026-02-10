4be2666eda6e80412b1e32ed0b86bac877d7140d
DevDyna <andrearisorti2003@gmail.com>
2026-02-10 23:45:47 +0100

    Update ExtraSlots.java

--- src/main/java/com/devdyna/justdynathings/client/core/ExtraSlots.java
+++ src/main/java/com/devdyna/justdynathings/client/core/ExtraSlots.java
@@ -2,10 +2,7 @@ package com.devdyna.justdynathings.client.core;
 
 import com.devdyna.justdynathings.Main;
 import com.devdyna.justdynathings.compat.jei.utils.Image;
-import com.devdyna.justdynathings.utils.LogUtil;
-
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.inventory.Slot;
 
 public interface ExtraSlots {

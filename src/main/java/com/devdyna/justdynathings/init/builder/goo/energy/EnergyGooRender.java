a60b19e68b820b0bd7f8974ea6b2fa3e5aff1bd4
DevDyna <andrearisorti2003@gmail.com>
2026-04-30 14:48:55 +0200

    fixed goo rendering not work

--- src/main/java/com/devdyna/justdynathings/init/builder/goo/energy/EnergyGooRender.java
+++ src/main/java/com/devdyna/justdynathings/init/builder/goo/energy/EnergyGooRender.java
@@ -1,14 +1,12 @@
 package com.devdyna.justdynathings.init.builder.goo.energy;
 
 import com.direwolf20.justdirethings.client.blockentityrenders.baseber.GooBlockRender_Base;
-import com.direwolf20.justdirethings.common.blocks.gooblocks.GooBlock_Base;
 import com.mojang.blaze3d.vertex.PoseStack;
 
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.feature.ModelFeatureRenderer.CrumblingOverlay;
 import net.minecraft.client.renderer.state.level.CameraRenderState;
-import net.minecraft.core.Direction;
 import net.minecraft.world.phys.Vec3;
 
 public class EnergyGooRender extends GooBlockRender_Base<FEGoo> {
@@ -16,28 +14,22 @@ public class EnergyGooRender extends GooBlockRender_Base<FEGoo> {
       super(context);
    }
 
-   //brute force to disable item revive render
+   public GooBlockRenderState createRenderState() {
+      return new GooBlockRenderState();
+   }
+
+   // brute force to disable item revive render
    @Override
    public void extractRenderState(FEGoo be, GooBlockRenderState render,
          float ticks, Vec3 cam, CrumblingOverlay breakprogress) {
-
-      render.renderBlockState = be.getBlockState();
-      render.alive = be.getBlockState().getValue(GooBlock_Base.ALIVE);
-      render.tier = be.getTier();
-      render.progress.clear();
-      for (Direction dir : Direction.values()) {
-         int remaining = be.getRemainingTimeFor(dir);
-         if (remaining > 0)
-            render.progress.put(dir, new int[] { remaining, be.getCraftingDuration(dir) });
-      }
-
+      super.extractRenderState(be, render, ticks, cam, breakprogress);
       render.showFloatingItem = false;
 
    }
 
    @Override
    public void submit(GooBlockRenderState arg0, PoseStack arg1, SubmitNodeCollector arg2, CameraRenderState arg3) {
-       super.submit(arg0, arg1, arg2, arg3);
+      super.submit(arg0, arg1, arg2, arg3);
    }
 
 }

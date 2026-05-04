d3ca075af3183d25af3c18b413a83357016e263d
DevDyna <andrearisorti2003@gmail.com>
2026-05-04 17:29:57 +0200

    Update RecipeSender.java

--- src/main/java/com/devdyna/justdynathings/common/RecipeSender.java
+++ src/main/java/com/devdyna/justdynathings/common/RecipeSender.java
@@ -1,13 +1,20 @@
 package com.devdyna.justdynathings.common;
 
+import com.devdyna.cakesticklib.api.utils.ModAddonUtil;
+import com.devdyna.justdynathings.init.types.zRecipeTypes;
+
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.OnDatapackSyncEvent;
 
 public class RecipeSender {
     @SubscribeEvent
     public static void onDatapackSync(OnDatapackSyncEvent event) {
-        // if (ModList.get().isLoaded("jei"))
-            // event.sendRecipes(zRecipeTypes.QUERN.getType());
+        if (ModAddonUtil.checkMod("jei")) {
+            event.sendRecipes(zRecipeTypes.FERRICORE_ANVIL.getType());
+            event.sendRecipes(zRecipeTypes.BLAZEGOLD_ANVIL.getType());
+            event.sendRecipes(zRecipeTypes.ECLIPSEALLOY_ANVIL.getType());
+
+        }
 
     }
 }

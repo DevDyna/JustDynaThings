61db866452e06e3ab27414c6b5243e607973a5e1
DevDyna <andrearisorti2003@gmail.com>
2026-05-09 20:24:23 +0200

    Create UnstableRawOre.java

---
+++ src/main/java/com/devdyna/justdynathings/init/builder/UnstableRawOre.java
@@ -0,0 +1,11 @@
+package com.devdyna.justdynathings.init.builder;
+
+import com.direwolf20.justdirethings.common.blocks.baseblocks.BaseRawOre;
+
+public class UnstableRawOre extends BaseRawOre {
+
+    public UnstableRawOre(Properties properties) {
+        super(properties);
+    }
+
+}

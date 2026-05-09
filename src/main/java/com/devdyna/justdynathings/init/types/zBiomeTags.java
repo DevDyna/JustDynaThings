6eedb54ee6e1b79d04b6b2e96d871aa855170d89
DevDyna <andrearisorti2003@gmail.com>
2026-05-09 20:24:13 +0200

    updated to use the merged lib method

--- src/main/java/com/devdyna/justdynathings/init/types/zBiomeTags.java
+++ src/main/java/com/devdyna/justdynathings/init/types/zBiomeTags.java
@@ -2,9 +2,8 @@ package com.devdyna.justdynathings.init.types;
 
 import static com.devdyna.justdynathings.JustDynaThings.MODULE_ID;
 
-import com.devdyna.cakesticklib.api.utils.x;
+import com.devdyna.cakesticklib.api.RegistryUtils;
 import com.devdyna.justdynathings.Constants;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.biome.Biome;
 import net.neoforged.bus.api.IEventBus;
@@ -15,22 +14,16 @@ public class zBiomeTags {
 
         }
 
-        // TODO move to api
-        @Deprecated
-        public static TagKey<Biome> tagBiome(String modname, String id) {
-                return TagKey.create(Registries.BIOME, x.rl(modname, id));
-        }
-
-        public static final TagKey<Biome> FERRICORE_SOLAR_PANEL_BIOME_LIST = tagBiome(MODULE_ID,
+        public static final TagKey<Biome> FERRICORE_SOLAR_PANEL_BIOME_LIST = RegistryUtils.tagBiome(MODULE_ID,
                         Constants.SolarPanelType + "s/" + Constants.Tiers.ferricore + "_allow");
 
-        public static final TagKey<Biome> BLAZEGOLD_SOLAR_PANEL_BIOME_LIST = tagBiome(MODULE_ID,
+        public static final TagKey<Biome> BLAZEGOLD_SOLAR_PANEL_BIOME_LIST = RegistryUtils.tagBiome(MODULE_ID,
                         Constants.SolarPanelType + "s/" + Constants.Tiers.blazegold + "_allow");
 
-        public static final TagKey<Biome> CELESTIGEM_SOLAR_PANEL_BIOME_LIST = tagBiome(MODULE_ID,
+        public static final TagKey<Biome> CELESTIGEM_SOLAR_PANEL_BIOME_LIST = RegistryUtils.tagBiome(MODULE_ID,
                         Constants.SolarPanelType + "s/" + Constants.Tiers.celestigem + "_allow");
 
-        public static final TagKey<Biome> ECLIPSEALLOY_SOLAR_PANEL_BIOME_LIST = tagBiome(MODULE_ID,
+        public static final TagKey<Biome> ECLIPSEALLOY_SOLAR_PANEL_BIOME_LIST = RegistryUtils.tagBiome(MODULE_ID,
                         Constants.SolarPanelType + "s/" + Constants.Tiers.eclipsealloy + "_allow");
 
 }

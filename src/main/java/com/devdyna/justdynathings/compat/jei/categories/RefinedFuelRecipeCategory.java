9f515d8bc9c30f84ac2098033fa1f85a797153f8
DevDyna <andrearisorti2003@gmail.com>
2026-05-17 15:47:32 +0200

    reworked fluid generator category to use 1mb intend of 1b

--- src/main/java/com/devdyna/justdynathings/compat/jei/categories/RefinedFuelRecipeCategory.java
+++ src/main/java/com/devdyna/justdynathings/compat/jei/categories/RefinedFuelRecipeCategory.java
@@ -4,27 +4,26 @@ import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.builder.ITooltipBuilder;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
-import mezz.jei.api.neoforge.NeoForgeTypes;
 import mezz.jei.api.recipe.IFocusGroup;
-import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.types.IRecipeType;
 
 import static com.devdyna.justdynathings.JustDynaThings.MODULE_ID;
 
 import com.devdyna.cakesticklib.api.compat.jei.BaseCategory;
-import com.devdyna.cakesticklib.api.compat.jei.ImageJei;
+import com.devdyna.cakesticklib.api.compat.jei.JEIFluidTankHelper;
 import com.devdyna.cakesticklib.api.primitive.Pos;
 import com.devdyna.cakesticklib.api.primitive.Size;
 import com.devdyna.cakesticklib.api.utils.x;
 import com.devdyna.justdynathings.JustDynaThings;
 import com.devdyna.justdynathings.compat.jei.utils.FuelRecords;
 import com.direwolf20.justdirethings.setup.JDTRegistration;
-import com.direwolf20.justdirethings.util.MagicHelpers;
-
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.level.ItemLike;
+import net.neoforged.neoforge.fluids.FluidStack;
+import net.neoforged.neoforge.fluids.crafting.FluidIngredient;
+import net.neoforged.neoforge.fluids.crafting.SizedFluidIngredient;
 
 @SuppressWarnings("null")
 public class RefinedFuelRecipeCategory extends BaseCategory<FuelRecords.Fluids> {
@@ -42,9 +41,13 @@ public class RefinedFuelRecipeCategory extends BaseCategory<FuelRecords.Fluids> 
 
     @Override
     public void setRecipe(IRecipeLayoutBuilder builder, FuelRecords.Fluids recipe, IFocusGroup focuses) {
-        builder.addSlot(RecipeIngredientRole.INPUT, 1, 8)
-                .addIngredients(NeoForgeTypes.FLUID_STACK,
-                        recipe.getFuels());
+        // TODO x.fluidSized shoud allow fluid lists
+        // TODO y+16
+        JEIFluidTankHelper.of()
+                .fluid(new SizedFluidIngredient(FluidIngredient.of(recipe.getFuels().toArray(FluidStack[]::new)), 1))
+                .offset(1, 8 + 16)
+                .build((x, y) -> builder.addInputSlot(x, y));
+
     }
 
     @Override
@@ -54,15 +57,7 @@ public class RefinedFuelRecipeCategory extends BaseCategory<FuelRecords.Fluids> 
 
     @Override
     public Size setXY() {
-        return Size.of(124, 32);
-    }
-
-    @Override
-    public void background(GuiGraphicsExtractor graphics) {
-        ImageJei.of()
-                .rl(this.setBackGround())
-                .size(96, this.getHeight())
-                .render(helper, graphics);
+        return Size.of(96, 32);
     }
 
     @Override
@@ -71,32 +66,29 @@ public class RefinedFuelRecipeCategory extends BaseCategory<FuelRecords.Fluids> 
             double mouseY) {
         super.draw(recipe, recipeSlotsView, guiGraphics, mouseX, mouseY);
 
-        var BUCKET = 1000;
+        // var BUCKET = 1000;
         var rate = recipe.getMultiplier();
-        var maxburn = BUCKET;
+        var maxburn = 1;
         var total = maxburn * rate;
 
         var stack = guiGraphics.pose();
         stack.pushMatrix();
         stack.scale(0.75F, 0.75F);
         guiGraphics.text(font,
-                Component.literal(hasShiftDown()
-                        ? MagicHelpers.ticksInSeconds(maxburn).replaceAll("\\.0$", "")
-                                + " sec"
-                        : maxburn + " ticks"),
+                Component.literal(maxburn + " ticks"),
                 46, 4,
                 0xFFFFFFFF);
         guiGraphics.text(font, Component.literal(rate + " FE/tick"), 46, 18, 0xFFFFFFFF);
-        guiGraphics.text(font, Component.literal((hasShiftDown() ? MagicHelpers.withSuffix(total) : total) + " FE"), 46,
+        guiGraphics.text(font, Component.literal(total + " FE"), 46,
                 32,
                 0xFFFFFFFF);
 
         stack.popMatrix();
 
-        ImageJei.of().rl("minecraft",
-                "textures/gui/sprites/icon/unseen_notification.png")
-                .size(10, 10).offset(100, 10)
-                .render(helper, guiGraphics);
+        // ImageJei.of().rl("minecraft",
+        // "textures/gui/sprites/icon/unseen_notification.png")
+        // .size(10, 10).offset(100, 10)
+        // .render(helper, guiGraphics);
 
     }
 
@@ -113,8 +105,8 @@ public class RefinedFuelRecipeCategory extends BaseCategory<FuelRecords.Fluids> 
         if (Pos.of(21, 22).setSize(10, 10).test(mouseX, mouseY))
             tooltip.add(Component.translatable(MODULE_ID + ".jei.total"));
 
-        if (Pos.of(100, 10).setSize(10, 10).test(mouseX, mouseY))
-            tooltip.add(Component.translatable(MODULE_ID + ".jei.bucket"));
+        // if (Pos.of(100, 10).setSize(10, 10).test(mouseX, mouseY))
+        // tooltip.add(Component.translatable(MODULE_ID + ".jei.bucket"));
 
     }
 

a2d0cb860fbb315651d6c069230440092d77931f
DevDyna <andrearisorti2003@gmail.com>
2026-08-12 00:57:20 +0200

    simplified solar panel screen classes

---
+++ src/main/java/com/devdyna/justdynathings/registry/builders/solar/SolarBaseScreen.java
@@ -0,0 +1,57 @@
+package com.devdyna.justdynathings.registry.builders.solar;
+
+import com.devdyna.justdynathings.api.client.ExtraSlots;
+import com.direwolf20.justdirethings.client.screens.basescreens.BaseMachineScreen;
+import com.direwolf20.justdirethings.client.screens.standardbuttons.ToggleButtonFactory;
+import com.direwolf20.justdirethings.client.screens.widgets.ToggleButton;
+import com.direwolf20.justdirethings.common.containers.basecontainers.BaseMachineContainer;
+import com.direwolf20.justdirethings.util.MiscHelpers;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+
+public abstract class SolarBaseScreen<T extends BaseMachineContainer> extends BaseMachineScreen<T>
+        implements ExtraSlots {
+
+    public SolarBaseScreen(T arg0, Inventory arg1, Component arg2) {
+        super(arg0, arg1, arg2);
+    }
+
+    @Override
+    public void init() {
+        super.init();
+    }
+
+    @Override
+    public void setTopSection() {
+        extraWidth = 0;
+        extraHeight = 0;
+    }
+
+    @Override
+    public void addRedstoneButtons() {
+        addRenderableWidget(ToggleButtonFactory.REDSTONEBUTTON(getGuiLeft() + 104, topSectionTop + 38,
+                redstoneMode.ordinal(), b -> {
+                    redstoneMode = MiscHelpers.RedstoneMode.values()[((ToggleButton) b).getTexturePosition()];
+                    saveSettings();
+                }));
+    }
+
+    @Override
+    public void addTickSpeedButton() {
+        // empty remove tick button
+    }
+
+    @Override
+    protected void drawMachineSlot(GuiGraphics guiGraphics, Slot slot) {
+        ItemStack itemStack = slot.getItem();
+        if (itemStack.isEmpty())
+            addSlotCharge(guiGraphics, slot);
+        else
+            super.drawMachineSlot(guiGraphics, slot);
+    }
+
+}
--- src/main/java/com/devdyna/justdynathings/registry/builders/solar/celestigem/CelestigemSolarPanelScreen.java
+++ src/main/java/com/devdyna/justdynathings/registry/builders/solar/celestigem/CelestigemSolarPanelScreen.java
@@ -1,52 +1,12 @@
 package com.devdyna.justdynathings.registry.builders.solar.celestigem;
 
-import com.devdyna.justdynathings.api.client.ExtraSlots;
-import com.direwolf20.justdirethings.client.screens.basescreens.BaseMachineScreen;
-import com.direwolf20.justdirethings.client.screens.standardbuttons.ToggleButtonFactory;
-import com.direwolf20.justdirethings.client.screens.widgets.ToggleButton;
-import com.direwolf20.justdirethings.util.MiscHelpers;
-import net.minecraft.client.gui.GuiGraphics;
+import com.devdyna.justdynathings.registry.builders.solar.SolarBaseScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.inventory.Slot;
-import net.minecraft.world.item.ItemStack;
 
-public class CelestigemSolarPanelScreen extends BaseMachineScreen<CelestigemSolarPanelGUI> implements ExtraSlots {
+public class CelestigemSolarPanelScreen extends SolarBaseScreen<CelestigemSolarPanelGUI> {
     public CelestigemSolarPanelScreen(CelestigemSolarPanelGUI container, Inventory inv, Component name) {
         super(container, inv, name);
     }
 
-    @Override
-    public void init() {
-        super.init();
-    }
-
-    @Override
-    public void setTopSection() {
-        extraWidth = 0;
-        extraHeight = 0;
-    }
-
-    @Override
-    public void addRedstoneButtons() {
-        addRenderableWidget(ToggleButtonFactory.REDSTONEBUTTON(getGuiLeft() + 104, topSectionTop + 38,
-                redstoneMode.ordinal(), b -> {
-                    redstoneMode = MiscHelpers.RedstoneMode.values()[((ToggleButton) b).getTexturePosition()];
-                    saveSettings();
-                }));
-    }
-
-    @Override
-    public void addTickSpeedButton() {
-        // empty remove tick button
-    }
-
-    @Override
-    protected void drawMachineSlot(GuiGraphics guiGraphics, Slot slot) {
-        ItemStack itemStack = slot.getItem();
-        if (itemStack.isEmpty())
-            addSlotCharge(guiGraphics, slot);
-        else
-            super.drawMachineSlot(guiGraphics, slot);
-    }
 }
